af0999720ccf8f3311a0ddff068a36c7d88bf8f5
FichteFoll <fichtefoll2@googlemail.com>
2017-12-20 03:44:04 +0100

    Final clean up on day 19

--- day-19/main2.rs
+++ day-19/main2.rs
@@ -27,14 +27,14 @@ enum Direction {
 }
 
 impl Direction {
-    pub fn iterator() -> Iter<'static, Direction> {
+    pub fn iterator() -> Iter<'static, Self> {
         use self::Direction::*;
-        static DIRECTIONS: [Direction;  4] = [RIGHT, DOWN, LEFT, UP];
+        static DIRECTIONS: [Direction; 4] = [RIGHT, DOWN, LEFT, UP];
         DIRECTIONS.into_iter()
     }
 
     fn value(&self) -> i8 {
-        // needed for comparisons
+        // needed for comparisons, notably checking for opposite direction
         use self::Direction::*;
         match *self {
             RIGHT => 1,
@@ -56,8 +56,7 @@ impl Direction {
         point.y = (point.y as isize + adj_y) as usize;
     }
 
-    fn next_point(&self, mut point: Point) -> Point {
-        // let mut next_point = (*point).clone();
+    fn next_point(&self, mut point: Point) -> Point {  // point is copied on invocation
         self.move_point(&mut point);
         point
     }
@@ -72,33 +71,25 @@ fn main() {
     let grid: Vec<Vec<char>> = input.split("\n").map(|l| l.chars().collect()).collect();
 
     // find start cell
-    let mut pos = Point{x: 0, y: 0};
-    for (i, cell) in grid[0].iter().enumerate() {
-        if *cell == '|' {
-            println!("{}, {}", i, cell);
-            pos = Point{x: i, y: 0};
-            break
-        }
-    }
-
+    let x = grid[0].iter().position(|&x| x == '|').unwrap();
+    let mut pos = Point{x , y: 0};
     println!("start pos: {:?}", pos);
-    assert!(pos != Point{x: 0, y: 0});
-    let mut direction = &Direction::DOWN;
 
+    let mut direction = &Direction::DOWN;
     // we start at 0 because we also count the last step into an empty cell
     let mut steps: usize = 0;
     let mut chars: Vec<char> = Vec::new();
     loop {
         direction.move_point(&mut pos);
         steps += 1;
-        let cell = &grid[pos.y][pos.x];
+        let cell = grid[pos.y][pos.x];
         println!("pos {:?}, cell {:?}; direction {:?}", pos, cell, direction);
-        match *cell {
+        match cell {
             ' ' => break, // reached the end
             '|' |
             '-' => continue,
             '+' => (),
-            _   => {chars.push(*cell); continue},
+            _   => {chars.push(cell); continue},
         }
 
         // need to determine new direction
@@ -107,14 +98,14 @@ fn main() {
                 continue
             }
             let test_pos = dir.next_point(pos);
-            let test_cell = &grid[test_pos.y][test_pos.x];
-            if *test_cell == ' ' {
+            let test_cell = grid[test_pos.y][test_pos.x];
+            if test_cell == ' ' {
                 continue
             }
             direction = dir;
             break
-        };
-    };
+        }
+    }
 
     let text: String = chars.iter().collect();
     println!("Collected text: {}", text);
