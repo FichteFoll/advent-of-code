f07059edd544351fdf9f5dccb479bcd234a89fdc
FichteFoll <fichtefoll2@googlemail.com>
2019-12-02 01:34:50 +0100

    2019 day 01 in Haskell

---
+++ 2019/day-01/day01.hs
@@ -0,0 +1,24 @@
+parse :: String -> [Int]
+parse = map read . lines
+
+part1 :: [Int] -> Int
+part1 = sum . map cost
+
+cost :: Int -> Int
+cost n = n `div` 3 - 2
+
+part2 :: [Int] -> Int
+part2 = sum . map costRec
+
+costRec :: Int -> Int
+costRec n
+    | m <= 0 = 0
+    | otherwise = m + costRec m
+    where m = n `div` 3 - 2
+
+
+main = do
+    contents <- getContents
+    let input = (parse contents)
+    putStrLn $ "Part 1: " ++ (show $ part1 input)
+    putStrLn $ "Part 2: " ++ (show $ part2 input)
