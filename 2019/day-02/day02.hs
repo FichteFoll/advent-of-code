29b16ec31a10728f369e61c75b00065bb859e6f2
FichteFoll <fichtefoll2@googlemail.com>
2019-12-03 02:51:26 +0100

    Revise 2019 day 02 script

--- 2019/day-02/day02.hs
+++ 2019/day-02/day02.hs
@@ -1,38 +1,35 @@
 import Debug.Trace (trace)
-import Data.List.Split
+import Data.List.Split (splitOn)
 
 
 parse :: String -> [Int]
 parse = map read . splitOn ","
 
 repl :: [Int] -> Int -> Int -> [Int]
-repl xs a b = (head xs):a:b:(drop 3 xs)
+repl (a:_:_:xs) b c = a:b:c:xs
 
-run :: Int -> [Int] -> [Int] -> Int
--- run i _ state | trace ("run " ++ show i ++ " " ++ show state) False = undefined
-run i (99:_) state = head state
-run i (op:a:b:out:_) state =
-    run new_i instr new_state
+run :: Int -> [Int] -> [Int]
+run i state = case drop i state of
+    (99:_) -> state
+    (op:a:b:out:_) ->
+        run new_i new_state
+        where
+            r = (opcode op) (state !! a) (state !! b)
+            ss = splitAt out state
+            new_state = fst ss ++ [r] ++ drop 1 (snd ss)
+            new_i = i + 4
     where
         opcode 1 = (+)
         opcode 2 = (*)
-        r = (opcode op) (state !! a) (state !! b)
-        ss = splitAt (out) state
-        new_state = fst ss ++ [r] ++ drop 1 (snd ss)
-        new_i = i + 4
-        instr = drop new_i new_state
 
 part1 :: [Int] -> Int
-part1 xs = run 0 xxs xxs
-    where xxs = repl xs 12 2
+part1 xs = head $ run 0 $ repl xs 12 2
 
 part2 :: [Int] -> Int
-part2 xs = head [100 * a + b
-                | a <- [0..99],
-                  b <- [0..99],
-                  let xxs = repl xs a b,
-                  let res = run 0 xxs xxs,
-                  res == 19690720]
+part2 xs = head [100 * n + v
+                | n <- [0..99]
+                , v <- [0..99]
+                , (head . run 0 $ repl xs n v) == 19690720]
 
 main :: IO ()
 main = do
