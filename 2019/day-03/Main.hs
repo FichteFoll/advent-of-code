d47ab36eb979e33c30c42a8156360440dced6f64
FichteFoll <fichtefoll2@googlemail.com>
2019-12-03 21:36:01 +0100

    2019 day 03 part 2

--- 2019/day-03/Main.hs
+++ 2019/day-03/Main.hs
@@ -2,7 +2,7 @@ module Main where
 
 import Debug.Trace (trace)
 import Data.List.Split (splitOn)
-import Data.List (sort)
+import qualified Data.List as List
 import Data.Set (Set)
 import qualified Data.Set as Set
 
@@ -37,7 +37,18 @@ parse = map parseLines . lines
     tocoord ('D', n) = Pt   0   n
     tocoord ('L', n) = Pt (-n)  0
 
--- build a list of points the wire covers
+iterLine :: IPoint -> [IPoint]
+iterLine (Pt 0 0) = []
+iterLine (Pt x 0) = [Pt i 0 | i <- rangeFromOne x]
+iterLine (Pt 0 y) = [Pt 0 i | i <- rangeFromOne y]
+
+rangeFromOne :: (Num a, Enum a, Ord a) => a -> [a]
+rangeFromOne i
+  | i < 0 = map negate [1..(-i)]
+  | i > 0 = [1..i]
+  | otherwise = []
+
+-- Build a set of points the wire covers
 used :: IPoint -> [IPoint] -> Set IPoint
 used start [] = Set.empty
 used start (cmd:cmds) = line `Set.union` used new_start cmds
@@ -45,25 +56,32 @@ used start (cmd:cmds) = line `Set.union` used new_start cmds
     line = Set.fromList $ map (pairwise (+) start) $ iterLine cmd
     new_start = pairwise (+) start cmd
 
-    iterLine :: IPoint -> [IPoint]
-    iterLine (Pt 0 0) = []
-    iterLine (Pt x 0) = [Pt i 0 | i <- saturatedRange x]
-    iterLine (Pt 0 y) = [Pt 0 i | i <- saturatedRange y]
+-- Determine step count until wire reaches given point
+steps :: IPoint -> [IPoint] -> Int
+steps target wire = stepCount (Pt 0 0) target wire 0
+  where
+    stepCount start target (cmd:cmds) count = case List.findIndex (== target) line of
+      Just i -> count + i + 1
+      Nothing -> stepCount newStart target cmds (count + (dist cmd))
+      where
+        line = map (pairwise (+) start) $ iterLine cmd
+        newStart = pairwise (+) start cmd
 
-    saturatedRange :: (Num a, Enum a, Ord a) => a -> [a]
-    saturatedRange i
-      | i < 0 = [i..(-1)]
-      | i > 0 = [1..i]
-      | otherwise = []
+intersections :: [[IPoint]] -> Set IPoint
+intersections input = foldl Set.intersection (head points) (tail points)
+  where points = map (used (Pt 0 0)) input
 
 part1 :: [[IPoint]] -> Int
-part1 input = head . sort $ dist <$> Set.toList intersections
+part1 input = Set.findMin $ Set.map dist $ intersections input
+
+part2 :: [[IPoint]] -> Int
+part2 input = head $ List.sort $ stepList
   where
-    intersections = foldl Set.intersection (head points) (tail points)
-    points = map (used (Pt 0 0)) input
+    stepList = [sum $ [steps target wire | wire <- input]
+               | target <- Set.toList $ intersections input]
 
 main :: IO ()
 main = do
   input <- parse <$> getContents
   putStrLn $ "Part 1: " ++ (show $ part1 input)
-  -- putStrLn $ "Part 2: " ++ (show $ part2 input)
+  putStrLn $ "Part 2: " ++ (show $ part2 input)
--- 2019/day-03/MainSpec.hs
+++ 2019/day-03/MainSpec.hs
@@ -8,7 +8,7 @@ import Main hiding (main)
 
 main :: IO ()
 main = do
-  -- input <- parse <$> readFile "input.txt"
+  input <- parse <$> readFile "input.txt"
   hspec $ do
     describe "parse" $ do
       it "parses input" $
@@ -16,24 +16,50 @@ main = do
           [[Pt 8 0,Pt 0 (-5),Pt (-5) 0,Pt 0 3],[Pt 0 (-7),Pt 6 0,Pt 0 4,Pt (-4) 0]]
 
     describe "used" $ do
+      let used' = used (Pt 0 0)
       it "determines used fields 1" $
-        used (Pt 0 0) [Pt 8 0] `shouldBe` Set.fromList [Pt i 0 | i <- [1..8]]
+        used' [Pt 8 0] `shouldBe` Set.fromList [Pt i 0 | i <- [1..8]]
       it "determines used fields 2" $
         used (Pt 0 0) [Pt 0 (-3)] `shouldBe` Set.fromList [Pt 0 i | i <- [(-3)..(-1)]]
       it "determines used fields with sequence" $
-        used (Pt 0 0) [Pt 0 1, Pt 1 0] `shouldBe` Set.fromList [Pt 0 1, Pt 1 1]
+        used' [Pt 0 1, Pt 1 0] `shouldBe` Set.fromList [Pt 0 1, Pt 1 1]
+
+    describe "steps" $ do
+      it "counts required steps 1" $
+        steps (Pt 0 6) [Pt 0 8] `shouldBe` 6
+
+    describe "steps" $ do
+      it "counts required steps 2" $
+        steps (Pt 1 1) [Pt 0 8,Pt 1 0,Pt 0 (-8)] `shouldBe` 16
+
+    let example1 = parse "R8,U5,L5,D3\nU7,R6,D4,L4"
+        example2 = parse "R75,D30,R83,U83,L12,D49,R71,U7,L72\nU62,R66,U55,R34,D71,R55,D58,R83"
+        example3 = parse "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\nU98,R91,D20,R16,D67,R40,U7,R15,U6,R7"
 
     describe "part 1" $ do
-      it "computes simple example" $
-        let input = parse "R8,U5,L5,D3\nU7,R6,D4,L4" in
-        part1 input `shouldBe` 6
+      it "computes simple example 1" $
+        part1 example1 `shouldBe` 6
 
-    describe "part 1" $ do
-      it "computes complex example 1" $
-        let input = parse "R75,D30,R83,U83,L12,D49,R71,U7,L72\nU62,R66,U55,R34,D71,R55,D58,R83" in
-        part1 input `shouldBe` 159
-
-    describe "part 1" $ do
       it "computes complex example 2" $
-        let input = parse "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\nU98,R91,D20,R16,D67,R40,U7,R15,U6,R7" in
-        part1 input `shouldBe` 135
+        part1 example2 `shouldBe` 159
+
+      it "computes complex example 3" $
+        part1 example3 `shouldBe` 135
+
+    describe "part 2" $ do
+      it "computes simple example 1" $
+        part2 example1 `shouldBe` 30
+
+      it "computes complex example 2" $
+        part2 example2 `shouldBe` 610
+
+      it "computes complex example 3" $
+        part2 example3 `shouldBe` 410
+
+    describe "part1" $ do
+      it "computes accepted result" $
+         part1 input `shouldBe` 232
+
+    describe "part2" $ do
+      it "computes accepted result" $
+         part2 input `shouldBe` 6084
