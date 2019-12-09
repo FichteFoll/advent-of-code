6b023ac4d9b182503e40f90074ef626f433c01c5
FichteFoll <fichtefoll2@googlemail.com>
2019-12-09 17:42:24 +0100

    2019 day 09: Use record syntax for IntcodeState

--- 2019/day-09/Main.hs
+++ 2019/day-09/Main.hs
@@ -5,35 +5,44 @@ import Data.List
 import Control.Applicative (liftA2)
 
 type Tape = [Int]
--- ((instr_pointer, tape), (input, output))
-type IntcodeState = ((Int, Tape), ([Int], [Int]))
+data IntcodeState = IState {
+  pointer :: Int,
+  tape :: Tape,
+  relOffset :: Int,
+  input :: [Int],
+  output :: [Int]
+} deriving (Show)
+
+newAmp :: IntcodeState
+newAmp = IState 0 [] 0 [] []
 
 parse :: String -> Tape
 parse = map read . splitOn ","
 
 hasTerminated :: IntcodeState -> Bool
-hasTerminated ((-1, _), _) = True
+hasTerminated IState{pointer = (-1)} = True
 hasTerminated _ = False
 
 needsInput :: IntcodeState -> Bool
-needsInput ((i, tape), (inp, _)) = (tape !! i) == 3 && null inp
+needsInput s = ((tape s) !! (pointer s)) == 3 && null (input s)
 
 run :: IntcodeState -> [Int] -> IntcodeState
-run (state, (oldInp, out)) input =
+run s newInput =
   head . dropWhile (not . liftA2 (||) hasTerminated needsInput)
-  $ iterate step (state, (oldInp ++ input, []))
+  $ iterate step $ s {input = input s ++ newInput}
 
 step :: IntcodeState -> IntcodeState
-step ((i, tape), io@(inp, out)) = case opcode of
-  1  -> ((i+4, write 2 $ binOp (+)), io) -- ADD
-  2  -> ((i+4, write 2 $ binOp (*)), io) -- MUL
-  3  -> ((i+2, write 0 $ head inp), (tail inp, out)) -- IN
-  4  -> ((i+2, tape), (inp, out ++ [head params])) -- OUT
-  5  -> ((jumpIf ((/=) 0), tape), io) -- JNZ
-  6  -> ((jumpIf ((==) 0), tape), io) -- JZ
-  7  -> ((i+4, write 2 $ binCmp (<)), io) -- LT
-  8  -> ((i+4, write 2 $ binCmp (==)), io) -- EQ
-  99 -> ((-1, tape), io) -- HALT
+step s@IState{ pointer = i, tape = tape } = case opcode of
+  1  -> s { pointer = i+4, tape = write 2 $ binOp (+) } -- ADD
+  2  -> s { pointer = i+4, tape = write 2 $ binOp (*) } -- MUL
+  3  -> s { pointer = i+2, tape = write 0 $ head (input s), input = tail $ input s } -- IN
+  4  -> s { pointer = i+2, output = output s ++ take 1 params } -- OUT
+  5  -> s { pointer = jumpIf ((/=) 0) } -- JNZ
+  6  -> s { pointer = jumpIf ((==) 0) } -- JZ
+  7  -> s { pointer = i+4, tape = write 2 $ binCmp (<) } -- LT
+  8  -> s { pointer = i+4, tape = write 2 $ binCmp (==) } -- EQ
+  -- 9  -> TODO -- RBO (relative base offset)
+  99 -> s { pointer = -1 } -- HALT
   where
     (opmode:rawParams) = drop i tape
     (dmodes, opcode) = quotRem opmode 100
@@ -42,6 +51,7 @@ step ((i, tape), io@(inp, out)) = case opcode of
     loadMode 0 = (!!) tape
     loadMode 1 = id
 
+    -- TODO increase size if not big enough
     write = replace tape . ((!!) rawParams)
     binOp op = foldl1 op $ take 2 params
     jumpIf pred
@@ -53,20 +63,17 @@ replace :: [a] -> Int -> a -> [a]
 replace xs i v = fst ss ++ [v] ++ drop 1 (snd ss)
   where ss = splitAt i xs
 
-newAmp :: Tape -> [Int] -> IntcodeState
-newAmp tape input = ((0, tape), (input, []))
-
 initAmps :: Tape -> [Int] -> [IntcodeState]
-initAmps tape phases = [newAmp tape [phase] | phase <- phases]
+initAmps tape phases = [newAmp {tape = tape, input = [phase]} | phase <- phases]
 
 -- First part of the tuple carries over output
 runSeq :: ([Int], [IntcodeState]) -> ([Int], [IntcodeState])
 runSeq (input, []) = (input, [])
-runSeq (input, (amp:amps)) = (nextInput, nextAmp:newAmps)
+runSeq (input, (amp:amps)) = (newInput, newAmp:newAmps)
   where
-    afterRun@(_, (_, out)) = run amp input
-    nextAmp = fmap ([] <$) afterRun
-    (nextInput, newAmps) = runSeq (out, amps)
+    afterRun = run amp input
+    (newInput, newAmps) = runSeq (output afterRun, amps)
+    newAmp = afterRun {output = []}
 
 computeSeqLoop :: Tape -> [Int] -> Int
 computeSeqLoop tape phases = last $ fst $ head
--- 2019/day-09/MainSpec.hs
+++ 2019/day-09/MainSpec.hs
@@ -6,11 +6,11 @@ import Main hiding (main)
 
 main :: IO ()
 main = do
-  input <- parse <$> readFile "input.txt"
+  -- input <- parse <$> readFile "input.txt"
   hspec $ do
-    let runAmp tape inp = run (newAmp tape []) inp
+    let runAmp tape inp = run (newAmp {tape = tape}) inp
     describe "run" $ do
-      let run' tape = newCode where ((_, newCode), _) = runAmp tape []
+      let run' intape = tape $ run (newAmp {tape = intape}) []
       it "computes simple example 1" $
          run' [1,0,0,0,99] `shouldBe` [2,0,0,0,99]
       it "computes simple example 2" $
@@ -28,7 +28,7 @@ main = do
         run' [1101,100,-1,4,0] `shouldBe` [1101,100,-1,4,99]
 
       context "when provided with input" $ do
-        let runInput n tape = head $ snd $ snd $ runAmp tape [n]
+        let runInput n intape = head $ output $ run (newAmp {tape = intape}) [n]
         it "calculates ((==) 8) (pos mode)" $ property $
           \n -> runInput n [3,9,8,9,10,9,4,9,99,-1,8] == fromEnum (n == 8)
         it "calculates ((<) 8) (pos mode)" $ property $
@@ -47,60 +47,3 @@ main = do
                          1002,21,125,20,4,20,1105,1,46,104, 999,1105,1,46,1101,1000,1,20,4,20,
                          1105,1,46,98,99] in
           \n -> runInput n program == 1000 + fromEnum (n > 8) - fromEnum (n < 8)
-
-    describe "computeSeqLoop & part1" $ do
-      context "for example 1" $ do
-        let tape = [3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0]
-            seq = [4,3,2,1,0]
-            result = 43210
-        it "computeSeqLoop runs sequence" $
-          computeSeqLoop tape seq `shouldBe` result
-        it "part1 finds correct result" $
-          part1 tape `shouldBe` result
-
-      context "for example 2" $ do
-        let tape = [3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0]
-            seq = [0,1,2,3,4]
-            result = 54321
-        it "computeSeqLoop runs sequence" $
-          computeSeqLoop tape seq `shouldBe` result
-        it "part1 finds correct result" $
-          part1 tape `shouldBe` result
-
-      context "for example 3" $ do
-        let tape = [3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0]
-            seq = [1,0,4,3,2]
-            result = 65210
-        it "computeSeqLoop runs sequence" $
-          computeSeqLoop tape seq `shouldBe` result
-        it "part1 finds correct result" $
-          part1 tape `shouldBe` result
-    
-    describe "part1" $ do
-      it "computes accepted result" $
-         part1 input `shouldBe` 75228
-
-    describe "computeSeqLoop & part2" $ do
-      context "for example 1" $ do
-        let tape = [3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5]
-            seq = [9,8,7,6,5]
-            result = 139629729
-        it "computeSeqLoop multi-runs sequence" $
-          computeSeqLoop tape seq `shouldBe` result
-        it "part2 finds correct result" $
-          part2 tape `shouldBe` result
-
-      context "for example 2" $ do
-        let tape = [3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,
-                    1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4, 53,1001,56,-1,56,1005,
-                    56,6,99,0,0,0,0,10]
-            seq = [9,7,8,5,6]
-            result = 18216
-        it "computeSeqLoop multi-runs sequence" $
-          computeSeqLoop tape seq `shouldBe` result
-        it "part2 finds correct result" $
-          part2 tape `shouldBe` result
-
-    describe "part2" $ do
-      it "computes accepted result" $
-         part2 input `shouldBe` 79846026
