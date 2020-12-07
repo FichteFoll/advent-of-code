5b8144963cf409e6bd10b4d84e3c26ea293b57bb
FichteFoll <fichtefoll2@googlemail.com>
2020-12-07 17:15:50 +0100

    2020 day 07 part 1

---
+++ 2020/src/bin/day07.rs
@@ -0,0 +1,135 @@
+#![feature(test, str_split_once)]
+
+use std::collections::{HashMap, HashSet, VecDeque};
+
+// use itertools::Itertools;
+
+#[macro_use]
+extern crate lazy_static;
+
+fn read_input() -> String {
+    std::fs::read_to_string("input/day07.txt").expect("can’t read file")
+}
+
+type Input = HashMap<String, HashMap<String, usize>>;
+
+fn parse_input(input_str: &str) -> Input {
+    input_str
+        .trim()
+        .split("\n")
+        .map(|line| {
+            let (color, contents) = line
+                .strip_suffix(".").expect("line did not have a dot")
+                .split_once(" bags contain ").expect("could not split");
+            let rules: HashMap<_, _> = contents.split(", ")
+                .filter_map(|rule| {
+                    if rule == "no other bags" {
+                        None
+                    } else {
+                        let mut words = rule.split_whitespace();
+                        let n: usize = words.next().unwrap().parse().unwrap();
+                        let sub_color = words.take(2).collect::<Vec<_>>().join(" ");
+                        Some((sub_color, n))
+                    }
+                })
+                .collect();
+            (color.to_string(), rules)
+        })
+        .collect()
+}
+
+fn part_1(input: &Input) -> usize {
+    // build a directed graph
+    let mut graph: HashMap<&str, HashSet<&str>> = HashMap::new();
+    for (color, sub_colors) in input {
+        for sub_color in sub_colors.keys() {
+            graph.entry(sub_color).or_default().insert(color);
+        }
+    }
+    // traverse graph
+    let mut queue: VecDeque<&str> = vec!["shiny gold"].into_iter().collect();
+    let mut seen = HashSet::new();
+    loop {
+        if let Some(color) = queue.pop_front() {
+            if !seen.insert(color) {
+                continue;
+            }
+            graph.get(color).map(|sub_colors| queue.extend(sub_colors));
+        } else {
+            break;
+        }
+    }
+    seen.len() - 1
+}
+
+fn part_2(_input: &Input) -> usize {
+    0
+}
+
+fn main() {
+    let input_str = read_input();
+    let input = parse_input(&input_str);
+
+    println!("Part 1: {}", part_1(&input));
+    println!("Part 2: {}", part_2(&input));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    extern crate test;
+    use test::Bencher;
+
+    lazy_static! {
+        static ref EXAMPLE_INPUT_STR: &'static str = "\
+            light red bags contain 1 bright white bag, 2 muted yellow bags.\n\
+            dark orange bags contain 3 bright white bags, 4 muted yellow bags.\n\
+            bright white bags contain 1 shiny gold bag.\n\
+            muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.\n\
+            shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.\n\
+            dark olive bags contain 3 faded blue bags, 4 dotted black bags.\n\
+            vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.\n\
+            faded blue bags contain no other bags.\n\
+            dotted black bags contain no other bags.\n\
+            ";
+    }
+
+    #[test]
+    fn test_part_1() {
+        let input = parse_input(&EXAMPLE_INPUT_STR);
+        assert_eq!(part_1(&input), 4);
+    }
+
+    // #[test]
+    // fn test_part_2() {
+    //     let input = parse_input(&EXAMPLE_INPUT_STR);
+    //     assert_eq!(part_2(&input), 6);
+    // }
+
+    #[bench]
+    fn bench_parse(b: &mut Bencher) {
+        let input_str = read_input();
+        b.iter(|| {
+            let _ = parse_input(&input_str);
+        });
+    }
+
+    #[bench]
+    fn bench_part_1(b: &mut Bencher) {
+        let input_str = read_input();
+        let input = parse_input(&input_str);
+        b.iter(|| {
+            assert_eq!(part_1(&input), 213);
+        });
+    }
+
+    // #[bench]
+    // fn bench_part_2(b: &mut Bencher) {
+    //     let input_str = read_input();
+    //     let input = parse_input(&input_str);
+    //     b.iter(|| {
+    //         assert_eq!(part_2(&input), 3290);
+    //     });
+    // }
+}
