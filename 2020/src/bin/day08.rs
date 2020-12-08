2dfa6059c359d8495e057a03fae62b4854372b75
FichteFoll <fichtefoll2@googlemail.com>
2020-12-09 00:44:16 +0100

    2020 day 08 part 2

--- 2020/src/bin/day08.rs
+++ 2020/src/bin/day08.rs
@@ -6,18 +6,22 @@ fn read_input() -> String {
     std::fs::read_to_string("input/day08.txt").expect("can’t read file")
 }
 
+#[derive(Clone)]
 struct Handheld<'a> {
     pub acc: i64,
     pub instructions: &'a [Instruction],
     pub pointer: i64,
     pub seen: HashSet<i64>,
+    pub replaced_instr: Option<i64>,
 }
 
+#[derive(Debug)]
 struct Instruction {
     pub op: Operation,
     pub arg: i64,
 }
 
+#[derive(Debug,PartialEq)]
 enum Operation {
     Acc,
     Jmp,
@@ -26,18 +30,53 @@ enum Operation {
 
 impl<'a> Handheld<'a> {
     fn new(instructions: &'a [Instruction]) -> Self {
-        Handheld { acc: 0, instructions, pointer: 0, seen: HashSet::new()}
+        Handheld { acc: 0, instructions, pointer: 0, seen: HashSet::new(), replaced_instr: None }
     }
 
-    fn find_cycle(&mut self) {
-        while self.seen.insert(self.pointer) {
-            let instr = &self.instructions[self.pointer as usize];
-            match instr.op {
-                Operation::Acc => {self.acc += instr.arg; self.pointer += 1},
-                Operation::Jmp => self.pointer = self.pointer + instr.arg,
-                Operation::Nop => self.pointer += 1,
+    fn execute_one(&mut self) {
+        use Operation::*;
+        let instr = &self.instructions[self.pointer as usize];
+        let op = if self.replaced_instr == Some(self.pointer) {
+            match &instr.op {
+                Jmp => &Nop,
+                Nop => &Jmp,
+                x => x,
             }
+        } else {
+            &instr.op
+        };
+        match op {
+            Acc => {self.acc += instr.arg; self.pointer += 1},
+            Jmp => self.pointer = self.pointer + instr.arg,
+            Nop => self.pointer += 1,
         }
+    }
+
+    fn is_done(&self) -> bool {
+        !(0..self.instructions.len() as i64).contains(&self.pointer)
+    }
+
+    fn execute_until_cycle_or_done(&mut self) {
+        while !self.is_done() && self.seen.insert(self.pointer) {
+            self.execute_one()
+        }
+    }
+
+    fn find_terminating(mut self) -> Self {
+        let mut replaced = HashSet::new();
+        while !self.is_done() && !self.seen.contains(&self.pointer) {
+            let is_not_acc = &self.instructions[self.pointer as usize].op != &Operation::Acc;
+            if self.replaced_instr.is_none() && is_not_acc && replaced.insert(self.pointer) {
+                let mut branch = self.clone();
+                branch.replaced_instr = Some(self.pointer);
+                branch.execute_until_cycle_or_done();
+                if branch.is_done() {
+                    return branch;
+                }
+            }
+            self.execute_one()
+        }
+        self
     }
 }
 
@@ -62,12 +101,15 @@ fn parse_input(input_str: &str) -> Input {
 
 fn part_1(input: &Input) -> i64 {
     let mut hh = Handheld::new(input);
-    hh.find_cycle();
+    hh.execute_until_cycle_or_done();
     hh.acc
 }
 
-fn part_2(_input: &Input) -> usize {
-    0
+fn part_2(input: &Input) -> i64 {
+    let mut hh = Handheld::new(input);
+    hh = hh.find_terminating();
+    assert!(hh.is_done());
+    hh.acc
 }
 
 fn main() {
@@ -103,11 +145,11 @@ mod tests {
         assert_eq!(part_1(&input), 5);
     }
 
-    // #[test]
-    // fn test_part_2() {
-    //     let input = parse_input(&EXAMPLE_INPUT_STR);
-    //     assert_eq!(part_2(&input), 32);
-    // }
+    #[test]
+    fn test_part_2() {
+        let input = parse_input(&EXAMPLE_INPUT_STR);
+        assert_eq!(part_2(&input), 8);
+    }
 
     #[bench]
     fn bench_parse(b: &mut Bencher) {
@@ -126,12 +168,12 @@ mod tests {
         });
     }
 
-    // #[bench]
-    // fn bench_part_2(b: &mut Bencher) {
-    //     let input_str = read_input();
-    //     let input = parse_input(&input_str);
-    //     b.iter(|| {
-    //         assert_eq!(part_2(&input), 38426);
-    //     });
-    // }
+    #[bench]
+    fn bench_part_2(b: &mut Bencher) {
+        let input_str = read_input();
+        let input = parse_input(&input_str);
+        b.iter(|| {
+            assert_eq!(part_2(&input), 1089);
+        });
+    }
 }
