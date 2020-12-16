c1c8f5f519d34727ba7a0b7b16ea4ea98a104797
FichteFoll <fichtefoll2@googlemail.com>
2020-12-16 21:28:36 +0100

    2020 day 15 part 2

--- 2020/src/bin/day15.rs
+++ 2020/src/bin/day15.rs
@@ -13,13 +13,13 @@ fn parse_input(input_str: &str) -> Input {
         .collect()
 }
 
-fn part_1(input: &Input) -> usize {
-    let mut last_spoken: Vec<Option<usize>> = vec![None; 2020];
+fn memory_game(input: &Input, rounds: usize) -> usize {
+    let mut last_spoken: Vec<Option<usize>> = vec![None; rounds];
     for (i, &n) in input.iter().enumerate().rev().skip(1) {
         last_spoken[n] = Some(i + 1);
     }
     let mut last_num = *input.last().unwrap();
-    for i in input.len()..2020 {
+    for i in input.len()..rounds {
         let previous_use = last_spoken[last_num];
         let next_num = match previous_use {
             Some(last_i) => i - last_i,
@@ -31,8 +31,12 @@ fn part_1(input: &Input) -> usize {
     last_num
 }
 
-fn part_2(_input: &Input) -> usize {
-    0
+fn part_1(input: &Input) -> usize {
+    memory_game(input, 2020)
+}
+
+fn part_2(input: &Input) -> usize {
+    memory_game(input, 30_000_000)
 }
 
 fn main() {
@@ -50,9 +54,9 @@ mod tests {
     const TEST_INPUT_STR: &str = "0,3,6";
 
     test!(part_1() == 436);
-    // test!(part_2() == 0);
+    test!(part_2() == 175594);
     bench!(part_1() == 206);
-    // bench!(part_2() == 0);
+    bench!(part_2() == 955);
 
     #[test]
     fn test_part_1_more() {
@@ -62,5 +66,15 @@ mod tests {
         assert_eq!(part_1(&parse_input("2,3,1")), 78);
         assert_eq!(part_1(&parse_input("3,2,1")), 438);
         assert_eq!(part_1(&parse_input("3,1,2")), 1836);
+    }
+
+    #[test]
+    fn test_part_2_more() {
+        assert_eq!(part_1(&parse_input("1,3,2")), 2578);
+        assert_eq!(part_1(&parse_input("2,1,3")), 3544142);
+        assert_eq!(part_1(&parse_input("1,2,3")), 261214);
+        assert_eq!(part_1(&parse_input("2,3,1")), 6895259);
+        assert_eq!(part_1(&parse_input("3,2,1")), 18);
+        assert_eq!(part_1(&parse_input("3,1,2")), 362);
     }
 }
