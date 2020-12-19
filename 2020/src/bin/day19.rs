b5944f492307608ab05946bd4d3ef4991afe022d
FichteFoll <fichtefoll2@googlemail.com>
2020-12-19 15:31:26 +0100

    2020 day 19: allow rules with dynamic length
    Slightly slower, but less complex and better suited for part 2.

--- 2020/src/bin/day19.rs
+++ 2020/src/bin/day19.rs
@@ -1,6 +1,6 @@
-#![feature(test, str_split_once)]
+#![feature(test, str_split_once, bool_to_option)]
 
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 
 use aoc2020::*;
 
@@ -9,76 +9,12 @@ const DAY: usize = 19;
 #[derive(Clone, Debug)]
 enum Rule {
     Terminal(u8),
-    // Sequence(Vec<usize>),
     Branch(Vec<Vec<usize>>),
 }
 use Rule::*;
 
 type Rules = HashMap<usize, Rule>;
-type RulesLen = HashMap<usize, (Rule, usize)>;
 type Input = (Rules, Vec<String>);
-
-struct Matcher {
-    rules: RulesLen,
-}
-
-impl Matcher {
-    fn new(rules: &Rules) -> Self {
-        let mut rules_length: HashMap<usize, (Rule, usize)> = HashMap::new();
-        for rule_i in rules.keys() {
-            Matcher::rule_len(&mut rules_length, rules, rule_i);
-        }
-        Matcher { rules: rules_length }
-    }
-
-    fn rule_len(mut rules_length: &mut RulesLen, rules: &Rules, rule_i: &usize) -> usize {
-        if let Some((_, len)) = rules_length.get(rule_i) {
-            return *len;
-        }
-        let rule = &rules[rule_i];
-        let len = match rule {
-            Terminal(_) => 1,
-            Branch(branches) => {
-                let lengths: HashSet<_> = branches.iter()
-                    .map(|seq| seq.iter()
-                        .map(|i| Matcher::rule_len(&mut rules_length, rules, i))
-                        .sum()
-                    )
-                    .collect();
-                assert_eq!(lengths.len(), 1, "expected static length in branch rule");
-                lengths.into_iter().next().unwrap()
-            },
-        };
-        rules_length.insert(*rule_i, (rule.clone(), len));
-        len
-    }
-
-    fn matches(&self, word: &[u8]) -> bool {
-        let (_, rule_len) = &self.rules[&0];
-        if word.len() != *rule_len {
-            return false;
-        }
-        self.matches_inner(&0, word)
-    }
-
-    fn matches_inner(&self, rule_i: &usize, word: &[u8]) -> bool {
-        let result = match &self.rules[rule_i].0 {
-            Terminal(b) => &word[0] == b,
-            Branch(branches) => branches.iter()
-                .any(|seq| {
-                    let mut offset = 0;
-                    for sub_i in seq.iter() {
-                        if !self.matches_inner(sub_i, &word[offset..]) {
-                            return false;
-                        }
-                        offset += self.rules[&sub_i].1;
-                    }
-                    true
-                }),
-        };
-        result
-    }
-}
 
 fn parse_input(input_str: &str) -> Input {
     let (rule_block, word_block) = input_str
@@ -107,10 +43,36 @@ fn parse_input(input_str: &str) -> Input {
     (rules, words)
 }
 
+fn matches(rules: &Rules, word: &[u8]) -> bool {
+    consume(rules, &0, word).map(|rem| rem.len() == 0).unwrap_or(false)
+}
+
+fn consume<'a>(rules: &Rules, rule_i: &usize, word: &'a [u8]) -> Option<&'a [u8]> {
+    match &rules[rule_i] {
+        Terminal(b) => match word.len() {
+            0 => None,
+            1 => (word[0] == *b).then_some(&[]),
+            _ => (word[0] == *b).then_some(&word[1..]),
+        },
+        Branch(branches) => branches.iter()
+            .find_map(|seq| {
+                // TODO scan?
+                let mut rem = word;
+                for sub_i in seq.iter() {
+                    if let Some(new_rem) = consume(rules, sub_i, &rem) {
+                        rem = new_rem;
+                    } else {
+                        return None;
+                    }
+                }
+                Some(rem)
+            }),
+    }
+}
+
 fn part_1(input: &Input) -> usize {
-    let matcher = Matcher::new(&input.0);
     input.1.iter()
-        .filter(|word| matcher.matches(&word.as_bytes()))
+        .filter(|word| matches(&input.0, &word.as_bytes()))
         .count()
 }
 
@@ -162,7 +124,6 @@ mod tests {
             5: \"b\"\
             ";
         let rules = parse_input(input_str).0;
-        let matcher = Matcher::new(&rules);
         for (word, expected) in [
             ("ababbb", true),
             ("abbbab", true),
@@ -171,7 +132,7 @@ mod tests {
             ("aaaabbb", false),
         ].iter() {
             println!("testing {}", word);
-            assert_eq!(matcher.matches(word.as_bytes()), *expected);
+            assert_eq!(matches(&rules, word.as_bytes()), *expected);
         }
     }
 }
