46c9959db5cf28134ac49204e150797245a1b429
FichteFoll <fichtefoll2@googlemail.com>
2020-12-22 14:08:58 +0100

    2020 day 22 part 1

---
+++ 2020/src/bin/day22.rs
@@ -0,0 +1,77 @@
+#![feature(test)]
+
+use std::collections::VecDeque;
+
+use aoc2020::*;
+
+const DAY: usize = 22;
+type Input = Vec<VecDeque<usize>>;
+
+fn parse_input(input_str: &str) -> Input {
+    input_str
+        .trim()
+        .split("\n\n")
+        .map(|deck| deck.split("\n").skip(1).map(|line| line.parse().unwrap()).collect())
+        .collect()
+}
+
+fn part_1(input: &Input) -> usize {
+    let mut decks = input.clone();
+    while !decks.iter().any(|d| d.len() == 0) { // TODO what about 3 players?
+        let cards: Vec<_> = decks.iter_mut().map(|d| d.pop_front().unwrap()).collect();
+        let max = cards.iter().max().unwrap();
+        for (card, d) in cards.iter().zip(decks.iter_mut()) {
+            if card == max {
+                d.push_back(*max);
+                d.extend(cards.iter().filter(|&c| c != max));
+            }
+        }
+    }
+    decks.iter()
+        .find(|d| d.len() != 0)
+        .unwrap()
+        .into_iter()
+        .rev()
+        .enumerate()
+        .map(|(i, card)| (i + 1) * card)
+        .sum()
+}
+
+fn part_2(_input: &Input) -> usize {
+    0
+}
+
+fn main() {
+    let input_str = read_input!();
+    let input = parse_input(&input_str);
+    println!("Part 1: {}", part_1(&input));
+    println!("Part 2: {}", part_2(&input));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    extern crate test;
+
+    const TEST_INPUT_STR: &str = "\
+        Player 1:\n\
+        9\n\
+        2\n\
+        6\n\
+        3\n\
+        1\n\
+        \n\
+        Player 2:\n\
+        5\n\
+        8\n\
+        4\n\
+        7\n\
+        10\n\
+        ";
+
+    test!(part_1() == 306);
+    // test!(part_2() == 0);
+    // bench_parse!(len, 0);
+    bench!(part_1() == 31308);
+    // bench!(part_2() == 0);
+}
