e0f7d8aacc0eb5b9fee0cfdcc1e0d90b62b1c33a
FichteFoll <fichtefoll2@googlemail.com>
2020-12-22 17:01:20 +0100

    2020 day 22 part 2

--- 2020/src/bin/day22.rs
+++ 2020/src/bin/day22.rs
@@ -1,8 +1,10 @@
-#![feature(test)]
+#![feature(test, bool_to_option)]
 
-use std::collections::VecDeque;
+use std::collections::{HashSet, VecDeque};
 
 use aoc2020::*;
+
+use itertools::Itertools;
 
 const DAY: usize = 22;
 type Input = Vec<VecDeque<usize>>;
@@ -15,19 +17,8 @@ fn parse_input(input_str: &str) -> Input {
         .collect()
 }
 
-fn part_1(input: &Input) -> usize {
-    let mut decks = input.clone();
-    while !decks.iter().any(|d| d.len() == 0) { // TODO what about 3 players?
-        let cards: Vec<_> = decks.iter_mut().map(|d| d.pop_front().unwrap()).collect();
-        let max = cards.iter().max().unwrap();
-        for (card, d) in cards.iter().zip(decks.iter_mut()) {
-            if card == max {
-                d.push_back(*max);
-                d.extend(cards.iter().filter(|&c| c != max));
-            }
-        }
-    }
-    decks.iter()
+fn winners_score(decks: Input) -> usize {
+    decks.into_iter()
         .find(|d| d.len() != 0)
         .unwrap()
         .into_iter()
@@ -37,8 +28,53 @@ fn part_1(input: &Input) -> usize {
         .sum()
 }
 
-fn part_2(_input: &Input) -> usize {
-    0
+fn part_1(input: &Input) -> usize {
+    let mut decks = input.clone();
+    while !decks.iter().any(|d| d.len() == 0) {
+        let cards: Vec<_> = decks.iter_mut().map(|d| d.pop_front().unwrap()).collect();
+        let max = cards.iter().max().unwrap();
+        for (card, d) in cards.iter().zip(decks.iter_mut()) {
+            if card == max {
+                d.push_back(*max);
+                d.extend(cards.iter().filter(|&c| c != max));
+            }
+        }
+    }
+    winners_score(decks)
+}
+
+fn recursive_combat(mut decks: Input, l: usize) -> Input {
+    let mut seen: HashSet<Input> = HashSet::new();
+    while !decks.iter().any(|d| d.len() == 0) {
+        if !seen.insert(decks.clone()) {
+            // report winner using fake decks
+            return vec![vec![1].into_iter().collect(), VecDeque::new()];
+        }
+        let cards: Vec<_> = decks.iter_mut().map(|d| d.pop_front().unwrap()).collect();
+        let winner = if decks.iter().zip(cards.iter()).all(|(d, &c)| d.len() >= c) {
+            let sub_decks = decks.iter()
+                .zip(cards.iter())
+                .map(|(d, &c)| d.iter().cloned().take(c).collect())
+                .collect();
+            recursive_combat(sub_decks, l + 1)
+                .into_iter()
+                .find_position(|d| d.len() != 0)
+                .unwrap().0
+        } else {
+            let max = cards.iter().max().unwrap();
+            cards.iter()
+                .find_position(|&c| c == max)
+                .unwrap().0
+        };
+        decks[winner].push_back(cards[winner]);
+        decks[winner].extend(cards.iter().enumerate().filter_map(|(i, c)| (i != winner).then_some(c)));
+    }
+    decks
+}
+
+fn part_2(input: &Input) -> usize {
+    let decks = recursive_combat(input.clone(), 0);
+    winners_score(decks)
 }
 
 fn main() {
@@ -70,8 +106,7 @@ mod tests {
         ";
 
     test!(part_1() == 306);
-    // test!(part_2() == 0);
-    // bench_parse!(len, 0);
+    test!(part_2() == 291);
     bench!(part_1() == 31308);
-    // bench!(part_2() == 0);
+    // bench!(part_2() == 33647); // takes almost 1s
 }
