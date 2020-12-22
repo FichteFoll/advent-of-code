ceb0d230db29e3ad1e2059f68a71ad1d51d887cd
FichteFoll <fichtefoll2@googlemail.com>
2020-12-22 13:47:24 +0100

    2020 day 21 part 2

--- 2020/src/bin/day21.rs
+++ 2020/src/bin/day21.rs
@@ -20,7 +20,7 @@ fn parse_input(input_str: &str) -> Input {
         .collect()
 }
 
-fn find_ingreds_without_allergens<'a>(input: &Input<'a>) -> HashSet<&'a str> {
+fn build_allergen_map<'a>(input: &Input<'a>) -> HashMap<&'a str, HashSet<&'a str>> {
     let mut reverse_input: HashMap<&str, HashSet<&str>> = HashMap::new();
     for (is, as_) in input {
         for a in as_ {
@@ -29,22 +29,51 @@ fn find_ingreds_without_allergens<'a>(input: &Input<'a>) -> HashSet<&'a str> {
                 .or_insert(is.iter().cloned().collect());
         }
     }
-    let mut ingreds: HashSet<&str> = input.iter().flat_map(|(xs, _)| xs.iter()).cloned().collect();
-    for is in reverse_input.values() {
-        ingreds.retain(|i| !is.contains(i));
-    }
-    ingreds
+    reverse_input
 }
 
 fn part_1(input: &Input) -> usize {
-    let ingreds = find_ingreds_without_allergens(input);
+    let allergen_map = build_allergen_map(input);
+    let mut ingreds: HashSet<&str> = input.iter().flat_map(|(xs, _)| xs.iter()).cloned().collect();
+    for is in allergen_map.values() {
+        ingreds.retain(|i| !is.contains(i));
+    }
     input.iter()
         .map(|(dish_ingreds, _)| dish_ingreds.iter().filter(|i| ingreds.contains(*i)).count())
         .sum()
 }
 
-fn part_2(_input: &Input) -> usize {
-    0
+fn resolve_multimap<'a>(multimap: &HashMap<&'a str, HashSet<&'a str>>, singlemap: HashMap<&'a str, &'a str>)
+-> Option<HashMap<&'a str, &'a str>> {
+    // recursive depth-first search
+    let mut sorted_map: Vec<_> = multimap.iter().collect();
+    sorted_map.sort_unstable_by_key(|(_, x)| x.len());
+    if let Some((&a, is)) = sorted_map.first() {
+        for i in is.iter() {
+            let mut new_singlemap = singlemap.clone();
+            new_singlemap.insert(a, i);
+            let mut new_multimap = multimap.clone();
+            for mis in new_multimap.values_mut() {
+                mis.retain(|x| x != i)
+            }
+            new_multimap.remove(a);
+            let result = resolve_multimap(&new_multimap, new_singlemap);
+            if result.is_some() {
+                return result;
+            }
+        }
+        None
+    } else {
+        Some(singlemap)
+    }
+}
+
+fn part_2(input: &Input) -> String {
+    let allergen_map = build_allergen_map(input);
+    let resolved_map = resolve_multimap(&allergen_map, HashMap::new()).unwrap();
+    let mut keys: Vec<_> = resolved_map.keys().cloned().collect();
+    keys.sort_unstable();
+    keys.into_iter().map(|k| resolved_map[k]).collect::<Vec<_>>().join(",")
 }
 
 fn main() {
@@ -67,8 +96,8 @@ mod tests {
         ";
 
     test!(part_1() == 5);
-    // test!(part_2() == 0);
+    test!(part_2() == "mxmxvkd,sqjhc,fvjkl");
     bench_parse!(len, 35);
     bench!(part_1() == 1913);
-    // bench!(part_2() == 0);
+    bench!(part_2() == "gpgrb,tjlz,gtjmd,spbxz,pfdkkzp,xcfpc,txzv,znqbr");
 }
