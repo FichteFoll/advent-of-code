35521ef5ece4677abb480db561ed89e3c5cd0ab4
FichteFoll <fichtefoll2@googlemail.com>
2020-12-27 13:53:54 +0100

    2020 day 23 part 2: WIP, way too slow and erroneous

--- 2020/src/bin/day23.rs
+++ 2020/src/bin/day23.rs
@@ -7,24 +7,27 @@ use aoc2020::*;
 use itertools::Itertools;
 
 const DAY: usize = 23;
-type Input = VecDeque<u8>;
+type Input = VecDeque<u32>;
 
 fn parse_input(input_str: &str) -> Input {
     input_str
         .trim()
         .bytes()
-        .map(|b| b - b'0')
+        .map(|b| (b - b'0') as u32)
         .collect()
 }
 
-fn part_1(input: &Input, rounds: usize) -> String {
-    let mut cups = input.clone();
-    for _ in 0..rounds {
+fn play_rounds(cups: &mut Input, rounds: usize) {
+    let max = *cups.iter().max().unwrap();
+    for r in 0..rounds {
+        if r % 10_000 == 0 {
+            println!("round {}", r);
+        }
         let current = cups.pop_front().unwrap();
         let to_sort: Vec<_> = (0..3).map(|_| cups.pop_front().unwrap()).collect();
         cups.push_front(current);
         let target_i = (1..).find_map(|i| {
-            let target = ((current + 9) - i - 1) % 9 + 1; // wrapping in u8
+            let target = ((current + max) - i - 1) % max + 1; // wrapping in unsigned integer
             if let Some((target_i, _)) = cups.iter().find_position(|&j| *j == target) {
                 Some(target_i)
             } else {
@@ -35,18 +38,32 @@ fn part_1(input: &Input, rounds: usize) -> String {
         cups.extend(to_sort.into_iter());
         cups.rotate_right(target_i + 3);
     }
+}
 
+fn part_1(input: &Input, rounds: usize) -> String {
+    let mut cups = input.clone();
+    play_rounds(&mut cups, rounds);
     let pos_1 = cups.iter().find_position(|&j| j == &1).unwrap().0;
     cups.rotate_left(pos_1);
-    cups.pop_front();
     cups.into_iter()
+        .skip(1)
         .map(|i| format!("{}", i))
         .collect::<Vec<_>>()
         .join("")
 }
 
-fn part_2(_input: &Input) -> usize {
-    0
+fn part_2(input: &Input) -> u64 {
+    let mut cups = input.clone();
+    let max = *cups.iter().max().unwrap();
+    cups.extend(max + 1..=1_000_000);
+    play_rounds(&mut cups, 1_000_000);
+    let pos_1 = cups.iter().find_position(|&j| j == &1).unwrap().0;
+    cups.rotate_left(pos_1);
+    cups.into_iter()
+        .skip(1)
+        .take(2)
+        .map(|x| x as u64)
+        .product()
 }
 
 fn main() {
@@ -64,7 +81,7 @@ mod tests {
     const TEST_INPUT_STR: &str = "389125467";
 
     test!(part_1(10) == "92658374");
-    // test!(part_2() == 0);
+    test!(part_2() == 149245887792);
     bench!(part_1(100) == "25468379");
     // bench!(part_2() == 0);
 }
