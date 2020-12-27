5653ebd73611d03524edf98d215fdeba46e3edab
FichteFoll <fichtefoll2@googlemail.com>
2020-12-28 00:26:48 +0100

    2020 day 23 part 2: WIP, make it even slower using a linked list

--- 2020/src/bin/day23.rs
+++ 2020/src/bin/day23.rs
@@ -1,13 +1,13 @@
-#![feature(test)]
+#![feature(test, linked_list_cursors)]
 
-use std::collections::VecDeque;
+use std::collections::{LinkedList, linked_list::CursorMut};
 
 use aoc2020::*;
 
 use itertools::Itertools;
 
 const DAY: usize = 23;
-type Input = VecDeque<u32>;
+type Input = LinkedList<u32>;
 
 fn parse_input(input_str: &str) -> Input {
     input_str
@@ -17,38 +17,55 @@ fn parse_input(input_str: &str) -> Input {
         .collect()
 }
 
+fn move_curser_to<T: Eq>(cursor: &mut CursorMut<T>, mut target: T) {
+    while cursor.current() != Some(&mut target) {
+        cursor.move_next();
+    }
+}
+
+fn rotate_to<T: Eq>(list: &mut LinkedList<T>, target: T) {
+    let mut rest = {
+        let mut cursor = list.cursor_front_mut();
+        move_curser_to(&mut cursor, target);
+        cursor.remove_current();
+        cursor.split_before()
+    };
+    list.append(&mut rest);
+}
+
+
 fn play_rounds(cups: &mut Input, rounds: usize) {
-    let max = *cups.iter().max().unwrap();
+    let max = cups.len() as u32;
     for r in 0..rounds {
         if r % 10_000 == 0 {
             println!("round {}", r);
         }
         let current = cups.pop_front().unwrap();
-        let to_sort: Vec<_> = (0..3).map(|_| cups.pop_front().unwrap()).collect();
-        cups.push_front(current);
-        let target_i = (1..).find_map(|i| {
-            let target = ((current + max) - i - 1) % max + 1; // wrapping in unsigned integer
-            if let Some((target_i, _)) = cups.iter().find_position(|&j| *j == target) {
-                Some(target_i)
-            } else {
-                None
+        {
+            let mut cursor = cups.cursor_front_mut();
+            let to_sort: Vec<_> = (0..).filter_map(|_| cursor.remove_current_as_list()).take(3).collect();
+            for i in 1.. {
+                let target = ((current + max) - i - 1) % max + 1; // wrapping in unsigned integer
+                if to_sort.iter().any(|node| node.contains(&target)) {
+                    continue;
+                } else {
+                    move_curser_to(&mut cursor, target);
+                    to_sort.into_iter().rev().for_each(|node| cursor.splice_after(node));
+                    break;
+                }
             }
-        }).unwrap();
-        cups.rotate_left(target_i + 1);
-        cups.extend(to_sort.into_iter());
-        cups.rotate_right(target_i + 3);
+        }
+        cups.push_back(current);
     }
 }
 
 fn part_1(input: &Input, rounds: usize) -> String {
     let mut cups = input.clone();
     play_rounds(&mut cups, rounds);
-    let pos_1 = cups.iter().find_position(|&j| j == &1).unwrap().0;
-    cups.rotate_left(pos_1);
+    rotate_to(&mut cups, 1);
     cups.into_iter()
-        .skip(1)
-        .map(|i| format!("{}", i))
-        .collect::<Vec<_>>()
+        .map(|n| format!("{}", n))
+        .collect_vec()
         .join("")
 }
 
@@ -56,11 +73,10 @@ fn part_2(input: &Input) -> u64 {
     let mut cups = input.clone();
     let max = *cups.iter().max().unwrap();
     cups.extend(max + 1..=1_000_000);
-    play_rounds(&mut cups, 1_000_000);
-    let pos_1 = cups.iter().find_position(|&j| j == &1).unwrap().0;
-    cups.rotate_left(pos_1);
+    play_rounds(&mut cups, 10_000);
+    // play_rounds(&mut cups, 1_000_000);
+    rotate_to(&mut cups, 1);
     cups.into_iter()
-        .skip(1)
         .take(2)
         .map(|x| x as u64)
         .product()
