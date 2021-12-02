3836d4d552e06aab7e88b0e96aa2f87c4b38730a
FichteFoll <fichtefoll2@googlemail.com>
2021-12-02 18:51:45 +0100

    2021: split test macros into a module

---
+++ 2021/src/test.rs
@@ -0,0 +1,68 @@
+// requires TEST_INPUT & parse_input
+#[macro_export]
+macro_rules! test {
+    ($part: ident ($($param: expr),*) == $expected:expr) => {
+        paste::paste! {
+            #[test]
+            fn [<test_ $part>]() {
+                let parsed = parse_input(TEST_INPUT);
+                assert_eq!($part(&parsed$(, $param)*), $expected);
+            }
+        }
+    };
+    ($input: expr, $part: ident ($($param: expr),*) == $expected:expr) => {
+        paste::paste! {
+            #[test]
+            fn [<test_ $part>]() {
+                let parsed = parse_input($input);
+                assert_eq!($part(&parsed$(, $param)*), $expected);
+            }
+        }
+    };
+    ($suffix: ident, $input: expr, $part: ident ($($param: expr),*) == $expected:expr) => {
+        paste::paste! {
+            #[test]
+            fn [<test_ $part _ $suffix>]() {
+                let parsed = parse_input($input);
+                assert_eq!($part(&parsed$(, $param)*), $expected);
+            }
+        }
+    };
+}
+
+// requires parse_input
+#[macro_export]
+macro_rules! bench {
+    ($part: ident ($($param: expr),*) == $expected:expr) => {
+        paste::paste! {
+            #[bench]
+            fn [<bench_ $part>](b: &mut test::Bencher) {
+                let input = read_input!();
+                let parsed = parse_input(&input);
+                b.iter(|| assert_eq!($part(test::black_box(&parsed) $(, $param)* ), $expected));
+            }
+        }
+    };
+}
+
+// requires parse_input
+#[macro_export]
+macro_rules! bench_parse {
+    ($fn:ident, $expected_len:expr) => {
+        #[bench]
+        fn bench_parse(b: &mut test::Bencher) {
+            let raw = read_input!();
+            b.iter(|| assert_eq!(parse_input(test::black_box(&raw)).$fn(), $expected_len));
+        }
+    };
+    () => {
+        #[bench]
+        fn bench_parse(b: &mut test::Bencher) {
+            let raw = read_input!();
+            b.iter(|| {
+                let _ = parse_input(test::black_box(&raw));
+            });
+        }
+    };
+}
+
