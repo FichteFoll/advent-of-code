6076dbf4696fc08acb550ab911aef69de7753019
FichteFoll <fichtefoll2@googlemail.com>
2021-12-07 16:37:43 +0100

    2021 day 07 part 2

--- 2021/src/bin/day07.rs
+++ 2021/src/bin/day07.rs
@@ -31,18 +31,30 @@ fn part_1(parsed: &Parsed) -> usize {
     let min = *parsed.iter().min().unwrap();
     let max = *parsed.iter().max().unwrap();
     (min..max)
-        .map(|target| calc_fuel(parsed, target))
+        .map(|target| calc_fuel_linearly(parsed, target))
         .min()
         .unwrap()
 }
 
-fn part_2(_parsed: &Parsed) -> usize {
-    0
-}
-
-fn calc_fuel(parsed: &Parsed, target: usize) -> usize {
+fn calc_fuel_linearly(parsed: &Parsed, target: usize) -> usize {
     parsed.iter()
         .map(|n| n.abs_diff(target))
+        .sum()
+}
+
+fn part_2(parsed: &Parsed) -> usize {
+    let min = *parsed.iter().min().unwrap();
+    let max = *parsed.iter().max().unwrap();
+    (min..max)
+        .map(|target| calc_fuel_quadratically(parsed, target))
+        .min()
+        .unwrap()
+}
+
+fn calc_fuel_quadratically(parsed: &Parsed, target: usize) -> usize {
+    parsed.iter()
+        .map(|n| n.abs_diff(target))
+        .map(|n| n * (n + 1) / 2)
         .sum()
 }
 
@@ -54,16 +66,22 @@ mod tests {
     const TEST_INPUT: &str = "16,1,2,0,4,2,7,1,2,14";
 
     #[test]
-    fn test_calc_fuel() {
+    fn test_calc_fuel_linearly() {
         let parsed = parse_input(TEST_INPUT);
-        assert_eq!(calc_fuel(&parsed, 1), 41);
-        assert_eq!(calc_fuel(&parsed, 3), 39);
-        assert_eq!(calc_fuel(&parsed, 10), 71);
+        assert_eq!(calc_fuel_linearly(&parsed, 1), 41);
+        assert_eq!(calc_fuel_linearly(&parsed, 3), 39);
+        assert_eq!(calc_fuel_linearly(&parsed, 10), 71);
+    }
+
+    #[test]
+    fn test_calc_fuel_quadratically() {
+        let parsed = parse_input(TEST_INPUT);
+        assert_eq!(calc_fuel_quadratically(&parsed, 2), 206);
     }
 
     test!(part_1() == 37);
-    // test!(part_2() == 0);
+    test!(part_2() == 168);
     bench_parse!(Vec::len, 1000);
     bench!(part_1() == 336040);
-    // bench!(part_2() == 0);
+    bench!(part_2() == 94813675);
 }
