505b024914f88ac878ccce734a3ef4b9b3cb8f32
FichteFoll <fichtefoll2@googlemail.com>
2021-12-09 13:15:56 +0100

    2021 day 09 part 2

--- 2021/src/bin/day09.rs
+++ 2021/src/bin/day09.rs
@@ -1,8 +1,11 @@
-#![feature(bool_to_option)]
 #![feature(test)]
+
+use std::collections::{VecDeque, BTreeSet};
 
 use aoc2021::*;
 use aoc2021::grid2d::Grid2D;
+use aoc2021::coord::Point;
+use itertools::Itertools;
 use parse::parse_input;
 
 const DAY: usize = 9;
@@ -29,18 +32,48 @@ mod parse {
 }
 
 fn part_1(parsed: &Parsed) -> usize {
-    parsed.iter_enumerate()
-        .filter_map(|(pt, n)|
-            pt.direct_neighbors().into_iter()
-                .filter_map(|pt| parsed.get(&pt))
-                .all(|m| n < m)
-                .then_some(*n as usize + 1)
-        )
+    low_points(parsed)
+        .map(|(_, &n)| n as usize + 1)
         .sum()
 }
 
-fn part_2(_parsed: &Parsed) -> usize {
-    0
+fn part_2(parsed: &Parsed) -> usize {
+    basins(parsed).iter()
+        .map(Vec::len)
+        .sorted_unstable()
+        .rev()
+        .take(3)
+        .product()
+}
+
+fn low_points(parsed: &Parsed) -> impl Iterator<Item=(Point<2>, &u8)> {
+    parsed.iter_enumerate()
+        .filter(|(pt, &n)|
+            pt.direct_neighbors().into_iter()
+                .filter_map(|pt| parsed.get(&pt))
+                .all(|&m| n < m)
+        )
+}
+
+fn basins(parsed: &Parsed) -> Vec<Vec<u8>> {
+    low_points(parsed)
+        .map(|(pt, _)| {
+            let mut nums = vec![*parsed.get(&pt).unwrap()];
+            let mut queue: VecDeque<_> = pt.direct_neighbors().into();
+            let mut pts = BTreeSet::new();
+            pts.insert(pt);
+            while let Some(cur_pt) = queue.pop_front() {
+                if !pts.insert(cur_pt.clone()) {
+                    continue;
+                }
+                if let Some(&n) = parsed.get(&cur_pt).filter(|&&n| n != 9) {
+                    nums.push(n);
+                    queue.extend(cur_pt.direct_neighbors());
+                }
+            }
+            nums
+        })
+        .collect()
 }
 
 #[cfg(test)]
@@ -57,8 +90,8 @@ mod tests {
         ";
 
     test!(part_1() == 15);
-    // test!(part_2() == 0);
+    test!(part_2() == 1134);
     bench_parse!(|x: &Grid2D<_>| x.size, (100, 100));
     bench!(part_1() == 452);
-    // bench!(part_2() == 0);
+    bench!(part_2() == 1263735);
 }
