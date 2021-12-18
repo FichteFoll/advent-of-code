5c39a536e135e4a264d1748d34cdf4f5ea1dcd32
FichteFoll <fichtefoll2@googlemail.com>
2021-12-18 19:22:00 +0100

    2021 day 18: implement magnitude

--- 2021/src/bin/day18.rs
+++ 2021/src/bin/day18.rs
@@ -267,8 +267,11 @@ impl SnailNum {
         }
     }
 
-    fn magnitude(self) -> usize {
-        todo!("calc magnitude")
+    fn magnitude(&self) -> usize {
+        match self {
+            Terminal(x) => *x,
+            Pair(left, right) => left.magnitude() * 3 + right.magnitude() * 2,
+        }
     }
 }
 
@@ -360,5 +363,17 @@ mod tests {
         let parsed = parse_input(input);
         let sum: SnailNum = parsed.into_iter().sum();
         assert_eq!(sum, expected);
+    }
+
+    #[test]
+    fn magnitude() {
+        let mag = |s: &str| s.parse::<SnailNum>().unwrap().magnitude();
+        assert_eq!(mag("[[1,2],[[3,4],5]]"), 143);
+        assert_eq!(mag("[[[[0,7],4],[[7,8],[6,0]]],[8,1]]"), 1384);
+        assert_eq!(mag("[[[[1,1],[2,2]],[3,3]],[4,4]]"), 445);
+        assert_eq!(mag("[[[[3,0],[5,3]],[4,4]],[5,5]]"), 791);
+        assert_eq!(mag("[[[[5,0],[7,4]],[5,5]],[6,6]]"), 1137);
+        assert_eq!(mag("[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]"), 3488);
+        assert_eq!(mag("[[[[6,6],[7,6]],[[7,7],[7,0]]],[[[7,7],[7,7]],[[7,8],[9,9]]]]"), 4140);
     }
 }
