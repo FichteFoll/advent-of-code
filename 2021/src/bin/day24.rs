bfae79f5aa06746fd332d9d690fc73d699783999
FichteFoll <fichtefoll2@googlemail.com>
2021-12-26 21:22:51 +0100

    2021 day 24 part 1

--- 2021/src/bin/day24.rs
+++ 2021/src/bin/day24.rs
@@ -1,6 +1,5 @@
+#![feature(bool_to_option)]
 #![feature(test)]
-
-use std::collections::VecDeque;
 
 use thiserror::Error;
 
@@ -115,64 +114,82 @@ mod parse {
 }
 
 fn part_1(parsed: &Parsed) -> i64 {
-    (1e14 as i64..1e15 as i64).rev()
-        .map(|n| (n, format!("{n}").bytes().map(|b| (b - b'0') as i64).collect::<Vec<_>>()))
-        .filter(|(_, digits)| digits.iter().any(|&d| d == 0))
-        .find(|(n, digits)| {
-            if n % (1e10 as i64) == 0 {
-                println!("{n}");
-            }
-            let mut monad = Monad::new(parsed, digits);
-            monad.compute();
-            monad.registers[3] == 0
-        })
-        .unwrap().0
+    let params = extract_params(parsed);
+    find_input(&params, 0, 0).unwrap().into_iter()
+        .rev()
+        .fold(0, |acc, digit| acc * 10 + digit)
 }
 
 fn part_2(_parsed: &Parsed) -> usize {
     todo!()
 }
 
-struct Monad {
-    registers: [i64; 4],
-    instructions: Vec<Instr>,
-    input: VecDeque<i64>, // pops front
+// All iterations follow the same pattern and differ in 3 numbers.
+// Extract these here so we can run a simplified version of the code
+// with early exits for invalid starting combinations
+// that will never result in a valid number.
+#[derive(Debug)]
+struct StepParams {
+    div_z: i64,
+    add_x: i64,
+    add_y: i64,
 }
 
-impl Monad {
-    fn new(instrs: &[Instr], input: &[i64]) -> Self {
-        Monad {
-            registers: [0; 4],
-            instructions: instrs.into(),
-            input: input.iter().cloned().collect(),
-        }
-    }
+const BLOCK_SIZE: usize = 18;
+const BLOCK_COUNT: usize = 14;
 
-    fn compute(&mut self) {
-        for instr in self.instructions.clone() {
-            self.step(instr);
-            // println!("registers: {:?}", self.registers);
-        }
-    }
+fn extract_params(parsed: &Parsed) -> Vec<StepParams> {
+    assert_eq!(parsed.len(), BLOCK_COUNT * BLOCK_SIZE, "Input didn't match expected format");
+    (0..BLOCK_COUNT)
+        .map(|i| {
+            let div_z = match parsed[i * BLOCK_SIZE + 4] {
+                Div(Register(3), Literal(n)) => n,
+                instr => panic!("Expected `div z …` at pos {}, found: {:?}", i * BLOCK_SIZE + 4, instr),
+            };
+            let add_x = match parsed[i * BLOCK_SIZE + 5] {
+                Add(Register(1), Literal(n)) => n,
+                instr => panic!("Expected `add x …` at pos {}, found: {:?}", i * BLOCK_SIZE + 5, instr),
+            };
+            let add_y = match parsed[i * BLOCK_SIZE + 15] {
+                Add(Register(2), Literal(n)) => n,
+                instr => panic!("Expected `add y …` at pos {}, found: {:?}", i * BLOCK_SIZE + 15, instr),
+            };
+            StepParams { div_z, add_x, add_y }
+        }).collect()
+}
 
-    fn step(&mut self, instr: Instr) {
-        // println!("executing {instr:?}");
-        match instr {
-            Inp(Register(r)) => self.registers[r] = self.input.pop_front().expect("ran out of input"),
-            Add(Register(r), param) => self.registers[r] += self.read(param),
-            Mul(Register(r), param) => self.registers[r] *= self.read(param),
-            Div(Register(r), param) => self.registers[r] /= self.read(param),
-            Mod(Register(r), param) => self.registers[r] %= self.read(param),
-            Eql(Register(r), param) => self.registers[r] = (self.registers[r] == self.read(param)) as i64,
-            _ => unreachable!(),
-        }
+fn find_input(params: &[StepParams], index: usize, z: i64) -> Option<Vec<i64>> {
+    if index == BLOCK_COUNT {
+        (z == 0).then(|| vec![])
+    } else {
+        (1..=9).rev()
+            .find_map(|i| {
+                calc_block(&params[index], z, i)
+                    .and_then(|new_z| find_input(params, index + 1, new_z))
+                    .map(|mut res| { res.push(i); res })
+            })
     }
+}
 
-    fn read(&self, param: Param) -> i64 {
-        match param {
-            Literal(n) => n,
-            Register(r) => self.registers[r],
-        }
+fn calc_block(params: &StepParams, z: i64, input: i64) -> Option<i64> {
+    assert_eq!(params.div_z == 26, params.add_x < 10);
+    // In each iteration, there is a check for whether the input matches
+    // the carry modulo 26 plus add_x.
+    // If it matches, the carry (z) is multiplied with 26,
+    // then the input + some constant is added.
+    //
+    // Throughout the entire code base, z is divided by 26 7 times
+    // (and by 1 the other 7 times),
+    // in the same blocks where add_x is smaller than 10,
+    // i.e. there is a possibility of z *not* being multiplied by 26.
+    // Thus it must hold that the inputs for these blocks prevent a multiplication,
+    // otherwise we will never reach 0 at the end.
+    //
+    // FWIW, add_x is never in 0..10, at least in my input.
+    if params.add_x < 10 {
+        (z % 26 + params.add_x == input).then_some(z / params.div_z)
+    } else {
+        Some(z * 26 + input + params.add_y)
     }
 }
 
@@ -181,16 +198,67 @@ mod tests {
     use super::*;
     extern crate test;
 
-    // const TEST_INPUT: &str = "\
-    //     ";
-
-    // test!(part_1() == 0);
-    // test!(part_2() == 0);
     bench_parse!(Vec::len, 252);
-    // bench!(part_1() == 0);
+    bench!(part_1() == 99995969919326);
     // bench!(part_2() == 0);
 
-    mod monad {
+}
+
+// Treating the given code as a blackbox and passing all inputs into it is obviously infeasible,
+// but I wrote it anyway (with tests), so I might as well keep it.
+// It's just not used in the actual solution.
+#[cfg(test)]
+mod monad {
+    use super::*;
+
+    use std::collections::VecDeque;
+
+
+    struct Monad {
+        registers: [i64; 4],
+        instructions: Vec<Instr>,
+        input: VecDeque<i64>, // pops front
+    }
+
+    impl Monad {
+        fn new(instrs: &[Instr], input: &[i64]) -> Self {
+            Monad {
+                registers: [0; 4],
+                instructions: instrs.into(),
+                input: input.iter().cloned().collect(),
+            }
+        }
+
+        fn compute(&mut self) {
+            for instr in self.instructions.clone() {
+                self.step(instr);
+                // println!("registers: {:?}", self.registers);
+            }
+        }
+
+        fn step(&mut self, instr: Instr) {
+            // println!("executing {instr:?}");
+            match instr {
+                Inp(Register(r)) => self.registers[r] = self.input.pop_front().expect("ran out of input"),
+                Add(Register(r), param) => self.registers[r] += self.read(param),
+                Mul(Register(r), param) => self.registers[r] *= self.read(param),
+                Div(Register(r), param) => self.registers[r] /= self.read(param),
+                Mod(Register(r), param) => self.registers[r] %= self.read(param),
+                Eql(Register(r), param) => self.registers[r] = (self.registers[r] == self.read(param)) as i64,
+                _ => unreachable!(),
+            }
+        }
+
+        fn read(&self, param: Param) -> i64 {
+            match param {
+                Literal(n) => n,
+                Register(r) => self.registers[r],
+            }
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
         use super::*;
 
         #[test]
