323d340c524d442062ff0768e68f59c74b528abd
FichteFoll <fichtefoll2@googlemail.com>
2022-12-01 18:03:56 +0100

    Prepare for 2022

---
+++ 2022/src/lib.rs
@@ -0,0 +1,32 @@
+// Provides helpful methods, data structures and macros
+// for at least more than one task.
+
+pub mod test;
+
+#[cfg(feature = "hash_fnv")]
+pub mod collections {
+    pub use fnv::{FnvHashSet as HashSet, FnvHashMap as HashMap};
+}
+
+#[cfg(not(feature = "hash_fnv"))]
+pub mod collections {
+    use std::collections::{self, hash_map::RandomState};
+    pub type HashMap<K, V> = collections::HashMap<K, V, RandomState>;
+    pub type HashSet<T> = collections::HashSet<T, RandomState>;
+}
+
+pub fn read_file(day: usize) -> String {
+    std::fs::read_to_string(
+        std::env::var("INPUT")
+            .unwrap_or(format!("input/day{:0>2}.txt", day)),
+    )
+    .unwrap()
+}
+
+// requires DAY
+#[macro_export]
+macro_rules! read_input {
+    () => {
+        read_file(DAY)
+    };
+}
