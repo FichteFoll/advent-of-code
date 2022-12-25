fb8493acc1f8ffcbab1e8367037bbde6a8178fbc
FichteFoll <fichtefoll2@googlemail.com>
2022-12-25 13:09:25 +0100

    2022 day 24 part 2

--- 2022/src/bin/day24.rs
+++ 2022/src/bin/day24.rs
@@ -7,57 +7,78 @@ use std::iter::once;
 use aoc2022::collections::*;
 use aoc2022::coord::Point;
 use aoc2022::*;
-use parse::parse_input;
 
 const DAY: usize = 24;
 
 // Represents the direction of a blizzard.
 type Blizzard = u8;
-// A 2-dimensional grid with a variable number of blizzards.
+// A 2-dimensional grid with a variable number of blizzards,
+// indexed by x, then y coordinate.
 type Grid = Vec<Vec<Option<Blizzard>>>;
-// both indexed by x, then y coordinate
-type Parsed = (Grid, Grid);
+
+#[derive(Clone)]
+struct Parsed {
+    horizontal: Grid,
+    vertical: Grid,
+    len_x: usize,
+    len_y: usize,
+    start_pt: Point<2>,
+    end_pt: Point<2>,
+}
 
 main!();
 
-mod parse {
-    use super::*;
-
-    pub fn parse_input(input: &str) -> Parsed {
-        let lines: Vec<_> = input.lines().collect();
-        let size = (lines[0].len() - 2, lines.len() - 2);
-        let mut horizontal: Grid = vec![vec![None; size.1]; size.0];
-        let mut vertical: Grid = horizontal.clone();
-        for (y, line) in lines[1..=size.1].iter().enumerate() {
-            for (x, &b) in line.as_bytes()[1..=size.0].into_iter().enumerate() {
-                match b {
-                    b'.' => (),
-                    b'>' | b'<' => horizontal[x][y] = Some(b),
-                    b'v' | b'^' => vertical[x][y] = Some(b),
-                    _ => panic!("unrecognized char {b}"),
-                }
+fn parse_input(input: &str) -> Parsed {
+    let lines: Vec<_> = input.lines().collect();
+    let size = (lines[0].len() - 2, lines.len() - 2);
+    let mut horizontal: Grid = vec![vec![None; size.1]; size.0];
+    let mut vertical: Grid = horizontal.clone();
+    for (y, line) in lines[1..=size.1].iter().enumerate() {
+        for (x, &b) in line.as_bytes()[1..=size.0].into_iter().enumerate() {
+            match b {
+                b'.' => (),
+                b'>' | b'<' => horizontal[x][y] = Some(b),
+                b'v' | b'^' => vertical[x][y] = Some(b),
+                _ => panic!("unrecognized char {b}"),
             }
         }
-        (horizontal, vertical)
+    }
+    let (len_x, len_y) = (horizontal.len(), horizontal[0].len());
+    Parsed {
+        horizontal,
+        vertical,
+        len_x,
+        len_y,
+        start_pt: Point([0, -1]),
+        end_pt: Point([len_x as i32 - 1, len_y as i32]),
     }
 }
 
 fn part_1(parsed: &Parsed) -> usize {
-    let start_pt = Point([0, -1]);
-    let end_pt = Point([parsed.0.len() as i32 - 1, parsed.0[0].len() as i32]);
+    solve(parsed, 0)
+}
 
-    let start = State {
-        steps: Reverse(0),
-        pt: start_pt,
-    };
-    // let mut best: HsahMap<Point<2>, usize> = Default::default();
+fn part_2(parsed: &Parsed) -> usize {
+    let mut parsed = parsed.clone();
+    let mut steps = 0;
+    for _ in 0..3 {
+        steps = solve(&parsed, steps);
+        std::mem::swap(&mut parsed.start_pt, &mut parsed.end_pt);
+    }
+    steps
+}
+
+fn solve(parsed: &Parsed, start_steps: usize) -> usize {
     let mut queue: BinaryHeap<_> = Default::default();
+    let start = State {
+        steps: Reverse(start_steps),
+        pt: parsed.start_pt,
+    };
     queue.push(start);
     let mut seen: HashSet<_> = Default::default();
-
     while let Some(current) = queue.pop() {
         for next_pt in once(current.pt).chain(current.pt.direct_neighbors()) {
-            if next_pt == end_pt {
+            if next_pt == parsed.end_pt {
                 return current.steps.0 + 1;
             }
             if is_free(parsed, &next_pt, current.steps.0 + 1) {
@@ -71,27 +92,22 @@ fn part_1(parsed: &Parsed) -> usize {
             }
         }
     }
-    unreachable!();
+    unreachable!()
 }
 
-fn part_2(_parsed: &Parsed) -> usize {
-    todo!()
-}
-
-fn is_free((horizontal, vertical): &Parsed, pt: &Point<2>, step: usize) -> bool {
-    let (len_x, len_y) = (horizontal.len(), horizontal[0].len());
-    if pt == &Point([0, -1]) {
+fn is_free(p: &Parsed, pt: &Point<2>, step: usize) -> bool {
+    if pt == &p.start_pt {
         return true;
-    } else if pt.x() < 0 || pt.y() < 0 || pt.x() as usize >= len_x || pt.y() as usize >= len_y {
+    } else if pt.x() < 0 || pt.y() < 0 || pt.x() as usize >= p.len_x || pt.y() as usize >= p.len_y {
         // Assumes we checked for the end point earlier
         return false;
     }
     let (x, y) = (pt.x() as usize, pt.y() as usize);
     // West and south directions use "math" to prevent underflows.
-    let east_free = horizontal[(x + step) % len_x][y] != Some(b'<');
-    let west_free = horizontal[(x + step * (len_x - 1)) % len_x][y] != Some(b'>');
-    let north_free = vertical[x][(y + step) % len_y] != Some(b'^');
-    let south_free = vertical[x][(y + step * (len_y - 1)) % len_y] != Some(b'v');
+    let east_free = p.horizontal[(x + step) % p.len_x][y] != Some(b'<');
+    let west_free = p.horizontal[(x + step * (p.len_x - 1)) % p.len_x][y] != Some(b'>');
+    let north_free = p.vertical[x][(y + step) % p.len_y] != Some(b'^');
+    let south_free = p.vertical[x][(y + step * (p.len_y - 1)) % p.len_y] != Some(b'v');
     west_free && east_free && south_free && north_free
 }
 
@@ -116,8 +132,8 @@ mod tests {
         ";
 
     test!(part_1() == 18);
-    // test!(part_2() == 0);
-    // bench_parse!(Vec::len, 0);
+    test!(part_2() == 54);
+    bench_parse!(|p: &Parsed| p.horizontal.len(), 120);
     bench!(part_1() == 305);
-    // bench!(part_2() == 0);
+    bench!(part_2() == 905);
 }
