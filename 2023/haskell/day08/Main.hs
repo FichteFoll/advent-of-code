863f98e8c106c0f1c3bdf45a148586ac79d3ea9b
FichteFoll <fichtefoll2@googlemail.com>
2023-12-09 19:27:06 +0100

    2023 day 08: use a `Map`

--- 2023/haskell/day08/Main.hs
+++ 2023/haskell/day08/Main.hs
@@ -2,9 +2,12 @@
 {-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 
 module Main (main, parse, part1, part2) where
-import Control.Arrow ((&&&))
 
-type Network = [(String, (String, String))]
+import Control.Arrow ((&&&))
+import qualified Data.Map as Map
+import Data.Map (Map, (!))
+
+type Network = Map String (String, String)
 type Input = (String, Network)
 
 main :: IO ()
@@ -14,7 +17,7 @@ main = do
   putStrLn $ "Part 2: " ++ show (part2 input)
 
 parse :: String -> Input
-parse = (head &&& map parseMapLine . drop 2) . lines
+parse = (head &&& Map.fromList . map parseMapLine . drop 2) . lines
   where
     parseMapLine = take 3 &&& (take 3 . drop 7 &&& take 3 . drop 12)
 
@@ -28,7 +31,7 @@ countSteps net pos (lr:lrs) = 1 + countSteps net (nextPos net lr pos) lrs
 part2 :: Input -> Int
 part2 (lrs, net) = countSteps2 net start (cycle lrs)
   where
-    start = [pos | (pos, _) <- net, last pos == 'A']
+    start = [pos | pos <- Map.keys net, last pos == 'A']
 
 countSteps2 :: Network -> [String] -> [Char] -> Int
 countSteps2 _ state _ | all ((== 'Z') . last) state = 0
@@ -37,9 +40,7 @@ countSteps2 net state (lr:lrs) = 1 + countSteps2 net state' lrs
     state' = map (nextPos net lr) state
 
 nextPos :: Network -> Char -> String -> String
-nextPos ((key, branch):net') lr pos
-  | key == pos = getter lr branch
-  | otherwise = nextPos net' lr pos
+nextPos net lr pos = getter lr $ net ! pos
   where
     getter 'L' = fst
     getter 'R' = snd
