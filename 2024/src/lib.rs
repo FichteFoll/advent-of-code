ce7bba90d2a3fc53a7540f8e8aff6936767fb5a1
FichteFoll <fichtefoll2@googlemail.com>
2024-12-01 15:35:25 +0100

    2024: add Rust skeleton

---
+++ 2024/src/lib.rs
@@ -0,0 +1,38 @@
+// Provides helpful methods, data structures and macros
+// for at least more than one task.
+
+#![allow(incomplete_features)]
+#![feature(generic_const_exprs)]
+
+// pub mod coord;
+pub mod test;
+
+#[cfg(feature = "fnv")]
+pub mod collections {
+    pub use fnv::{FnvHashMap as HashMap, FnvHashSet as HashSet};
+}
+
+#[cfg(not(feature = "fnv"))]
+pub mod collections {
+    use std::collections::{self, hash_map::RandomState};
+    pub type HashMap<K, V> = collections::HashMap<K, V, RandomState>;
+    pub type HashSet<T> = collections::HashSet<T, RandomState>;
+}
+
+pub fn read_file(day: usize) -> String {
+    let path = std::env::var("INPUT").unwrap_or(format!("input/day{day:0>2}.txt"));
+    std::fs::read_to_string(path).unwrap()
+}
+
+// requires `DAY` & `parse_input`
+#[macro_export]
+macro_rules! main {
+    () => {
+        fn main() {
+            let input = read_file(DAY);
+            let parsed = parse_input(&input);
+            println!("Part 1: {}", part_1(&parsed));
+            println!("Part 2: {}", part_2(&parsed));
+        }
+    };
+}
---
+++ 2024/src/test.rs
@@ -0,0 +1,70 @@
+// requires `TEST_INPUT` & `parse_input`
+#[macro_export]
+macro_rules! test {
+    ($part: ident ($($param: expr),*) == $expected:expr) => {
+        paste::paste! {
+            #[test]
+            fn [<test_ $part>]() {
+                let parsed = parse_input(TEST_INPUT);
+                assert_eq!($part(&parsed$(, $param)*), $expected);
+            }
+        }
+    };
+    ($input: expr, $part: ident ($($param: expr),*) == $expected:expr) => {
+        paste::paste! {
+            #[test]
+            fn [<test_ $part>]() {
+                let parsed = parse_input($input);
+                assert_eq!($part(&parsed$(, $param)*), $expected);
+            }
+        }
+    };
+    ($suffix: ident, $input: expr, $part: ident ($($param: expr),*) == $expected:expr) => {
+        paste::paste! {
+            #[test]
+            fn [<test_ $part _ $suffix>]() {
+                let parsed = parse_input($input);
+                assert_eq!($part(&parsed$(, $param)*), $expected);
+            }
+        }
+    };
+}
+
+// requires `DAY` & `parse_input`
+#[macro_export]
+macro_rules! bench {
+    ($part: ident ($($param: expr),*) == $expected:expr) => {
+        paste::paste! {
+            #[bench]
+            fn [<bench_ $part>](b: &mut test::Bencher) {
+                let input = read_file(DAY);
+                let parsed = parse_input(&input);
+                b.iter(|| assert_eq!($part(test::black_box(&parsed) $(, $param)* ), $expected));
+            }
+        }
+    };
+}
+
+// requires `DAY` & `parse_input`
+#[macro_export]
+macro_rules! bench_parse {
+    ($fn:expr, $expected:expr) => {
+        #[bench]
+        fn bench_parse(b: &mut test::Bencher) {
+            let raw = read_file(DAY);
+            b.iter(|| {
+                let parsed = parse_input(test::black_box(&raw));
+                assert_eq!($fn(&parsed), $expected)
+            });
+        }
+    };
+    () => {
+        #[bench]
+        fn bench_parse(b: &mut test::Bencher) {
+            let raw = read_file(DAY);
+            b.iter(|| {
+                let _ = parse_input(test::black_box(&raw));
+            });
+        }
+    };
+}
