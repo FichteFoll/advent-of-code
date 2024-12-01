a19e9e2da6a611715faba5a3d6206da8130b7fda
FichteFoll <fichtefoll2@googlemail.com>
2024-12-01 17:13:48 +0100

    2024 day 01 part 1

---
+++ 2024/src/bin/day01.rs
@@ -0,0 +1,51 @@
+#![feature(test)]
+
+use aoc2024::*;
+use itertools::Itertools;
+
+const DAY: usize = 1;
+
+type Parsed = Vec<(i32, i32)>;
+
+main!();
+
+pub fn parse_input(input: &str) -> Parsed {
+    input
+        .lines()
+        .map(|line| {
+            let pair_s = line.split_once("   ").unwrap();
+            (pair_s.0.parse().unwrap(), pair_s.1.parse().unwrap())
+        })
+        .collect()
+}
+
+fn part_1(parsed: &Parsed) -> i32 {
+    let nums1 = parsed.iter().map(|(x, _)| x).sorted_unstable();
+    let nums2 = parsed.iter().map(|(_, x)| x).sorted_unstable();
+    nums1.zip(nums2).map(|(n1, n2)| (n1 - n2).abs()).sum()
+}
+
+fn part_2(_parsed: &Parsed) -> usize {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    extern crate test;
+
+    const TEST_INPUT: &str = "\
+        3   4\n\
+        4   3\n\
+        2   5\n\
+        1   3\n\
+        3   9\n\
+        3   3\n\
+        ";
+
+    test!(part_1() == 11);
+    // test!(part_2() == 0);
+    // bench_parse!(Vec::len, 0);
+    bench!(part_1() == 2264607);
+    // bench!(part_2() == 0);
+}
