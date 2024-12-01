c04bf6dc7aa5a0a4a761715c7b3a197a6e9ca52d
FichteFoll <fichtefoll2@googlemail.com>
2024-12-01 17:32:12 +0100

    2024 day 01: collect into two vectors

--- 2024/src/bin/day01.rs
+++ 2024/src/bin/day01.rs
@@ -5,29 +5,28 @@ use itertools::Itertools;
 
 const DAY: usize = 1;
 
-type Parsed = Vec<(i32, i32)>;
+type Parsed = (Vec<i32>, Vec<i32>);
 
 main!();
 
 pub fn parse_input(input: &str) -> Parsed {
-    input
-        .lines()
-        .map(|line| {
-            let pair_s = line.split_once("   ").unwrap();
-            (pair_s.0.parse().unwrap(), pair_s.1.parse().unwrap())
-        })
-        .collect()
+    input.lines().fold((vec![], vec![]), |mut vs, line| {
+        let pair_s = line.split_once("   ").unwrap();
+        vs.0.push(pair_s.0.parse().unwrap());
+        vs.1.push(pair_s.1.parse().unwrap());
+        vs
+    })
 }
 
 fn part_1(parsed: &Parsed) -> i32 {
-    let nums1 = parsed.iter().map(|(x, _)| x).sorted_unstable();
-    let nums2 = parsed.iter().map(|(_, x)| x).sorted_unstable();
+    let nums1 = parsed.0.iter().sorted_unstable();
+    let nums2 = parsed.1.iter().sorted_unstable();
     nums1.zip(nums2).map(|(n1, n2)| (n1 - n2).abs()).sum()
 }
 
 fn part_2(parsed: &Parsed) -> usize {
-    let nums1 = parsed.iter().map(|(x, _)| x).sorted_unstable();
-    let nums2_map = parsed.iter().map(|(_, x)| x).counts_by(|x| x);
+    let nums1 = parsed.0.iter().sorted_unstable();
+    let nums2_map = parsed.1.iter().counts_by(|x| x);
     nums1.map(|n| *n as usize * nums2_map.get(n).unwrap_or(&0)).sum()
 }
 
@@ -47,7 +46,7 @@ mod tests {
 
     test!(part_1() == 11);
     test!(part_2() == 31);
-    bench_parse!(Vec::len, 1000);
+    bench_parse!(|vs: &Parsed| (vs.0.len(), vs.1.len()), (1000, 1000));
     bench!(part_1() == 2264607);
     bench!(part_2() == 19457120);
 }
