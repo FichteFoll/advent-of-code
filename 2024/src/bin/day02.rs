476cdc12f5617f41dd88afa35dd49196ab10cb3f
FichteFoll <fichtefoll2@googlemail.com>
2024-12-02 23:06:25 +0100

    2024 day part 2

--- 2024/src/bin/day02.rs
+++ 2024/src/bin/day02.rs
@@ -25,25 +25,37 @@ fn parse_input(input: &str) -> Parsed {
 }
 
 fn part_1(parsed: &Parsed) -> usize {
+    parsed.iter().filter(|line| is_safe(line, &(1..=3))).count()
+}
+
+fn part_2(parsed: &Parsed) -> usize {
     parsed
         .iter()
-        .filter(|line| line_is_safe(line, 1..=3))
+        .filter(|line| is_safe_p2(line, &(1..=3)))
         .count()
-        + parsed
-            .iter()
-            .filter(|line| line_is_safe(line, -3..=-1))
-            .count()
 }
 
-fn part_2(_parsed: &Parsed) -> usize {
-    todo!()
-}
-
-fn line_is_safe(line: &[i32], safe_range: RangeInclusive<i32>) -> bool {
-    line.iter()
+fn is_safe(line: &[i32], safe_range: &RangeInclusive<i32>) -> bool {
+    line.into_iter()
         .tuple_windows()
-        .map(|(a, b)| a - b)
-        .all(|diff| safe_range.contains(&diff))
+        .map(|(a, b)| {
+            Some(b - a)
+                .filter(|d| safe_range.contains(&d.abs()))
+                .map(i32::signum)
+        })
+        .all_equal_value()
+        .unwrap_or(None)
+        .is_some()
+}
+
+fn is_safe_p2(line: &[i32], safe_range: &RangeInclusive<i32>) -> bool {
+    (0..=line.len()).any(|i| {
+        let mut sub_line = line.iter().cloned().collect_vec();
+        if i < line.len() {
+            sub_line.remove(i);
+        }
+        is_safe(&sub_line, safe_range)
+    })
 }
 
 #[cfg(test)]
@@ -61,8 +73,13 @@ mod tests {
         ";
 
     test!(part_1() == 2);
-    // test!(part_2() == 0);
+    test!(part_2() == 4);
     bench_parse!(Vec::len, 1000);
-    // bench!(part_1() == 0);
-    // bench!(part_2() == 0);
+    bench!(part_1() == 299);
+    bench!(part_2() == 364);
+
+    #[test]
+    pub fn test_p2_removes_correct_number() {
+        assert!(is_safe_p2(&[1, 3, 2, 3, 4], &(1..=3)));
+    }
 }
