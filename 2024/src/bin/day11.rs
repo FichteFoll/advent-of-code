fe2f01dc4f19500d405f9536021eebc022ff1d9b
FichteFoll <fichtefoll2@googlemail.com>
2024-12-11 18:16:27 +0100

    2024 day 11 part 2

--- 2024/src/bin/day11.rs
+++ 2024/src/bin/day11.rs
@@ -1,6 +1,7 @@
 #![feature(test)]
 
 use aoc2024::*;
+use collections::HashMap;
 
 const DAY: usize = 11;
 
@@ -16,35 +17,42 @@ fn parse_input(input: &str) -> Parsed {
         .collect()
 }
 
-fn part_1(parsed: &Parsed) -> usize {
-    let mut stones = parsed.clone();
-    for _ in 0..25 {
-        stones = blink(stones);
-    }
-    stones.len()
+fn part_1(parsed: &Parsed) -> I {
+    solve(parsed, 25)
 }
 
-fn blink(prev: Parsed) -> Parsed {
-    let mut stones = Vec::with_capacity(prev.len());
-    for s in prev {
-        if s == 0 {
-            stones.push(1);
-            continue;
-        }
-        let ndigits = s.ilog10() + 1;
+fn part_2(parsed: &Parsed) -> I {
+    solve(parsed, 75)
+}
+
+fn solve(stones: &Parsed, steps: usize) -> I {
+    let mut cache: HashMap<_, _> = Default::default();
+    stones
+        .iter()
+        .map(move |&s| blink_rec(&mut cache, s, steps))
+        .sum()
+}
+
+fn blink_rec(cache: &mut HashMap<(I, usize), I>, stone: I, rem: usize) -> I {
+    let key = (stone, rem);
+    let res = if rem == 0 {
+        1
+    } else if let Some(&x) = cache.get(&key) {
+        x
+    } else if stone == 0 {
+        blink_rec(cache, 1, rem - 1)
+    } else {
+        let ndigits = stone.ilog10() + 1;
         if ndigits % 2 == 0 {
             let divider = (10 as I).pow(ndigits / 2);
-            stones.push(s / divider);
-            stones.push(s % divider);
+            blink_rec(cache, stone / divider, rem - 1)
+                + blink_rec(cache, stone % divider, rem - 1)
         } else {
-            stones.push(s * 2024);
+            blink_rec(cache, stone * 2024, rem - 1)
         }
-    }
-    stones
-}
-
-fn part_2(_parsed: &Parsed) -> usize {
-    todo!()
+    };
+    cache.insert(key, res);
+    res
 }
 
 #[cfg(test)]
@@ -58,5 +66,5 @@ mod tests {
     // test!(part_2() == 0);
     bench_parse!(Vec::len, 8);
     bench!(part_1() == 224529);
-    // bench!(part_2() == 0);
+    bench!(part_2() == 266820198587914);
 }
