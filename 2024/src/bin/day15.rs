b8fbab097b5232cbeff17f67b417e9492f4e09a7
FichteFoll <fichtefoll2@googlemail.com>
2024-12-15 19:42:04 +0100

    2024 day 15 part 2

--- 2024/src/bin/day15.rs
+++ 2024/src/bin/day15.rs
@@ -1,5 +1,7 @@
 #![feature(test)]
 
+use std::collections::VecDeque;
+
 use itertools::Itertools;
 
 use aoc2024::*;
@@ -33,38 +35,108 @@ fn part_1((grid, instr): &Parsed) -> i32 {
     let mut grid = grid.clone();
     let mut robot = grid.position(|c| c == &'@').unwrap();
     for dir in instr {
-        let Some(free_i) = (1i32..)
-            .into_iter()
-            .map_while(|i| {
-                grid.get(&(robot + dir * i))
-                    .filter(|c| c != &&'#')
-                    .map(|b| (i, b))
-            })
-            .find_map(|(i, b)| (b == &'.').then_some(i))
-        else {
-            continue;
-        };
-        // Move everything towards the free block.
-        // (realistically I only need to do at most 2 swaps,
-        // but I cba to consider the edge case of free_i = 1.
-        for (i1, i2) in (0..=free_i).rev().tuple_windows() {
-            let (pt1, pt2) = (robot + dir * i1, robot + dir * i2);
-            grid.swap(&pt1, &pt2);
+        if move_simple(&mut grid, robot, dir) {
+            robot += dir;
         }
-        robot += dir;
     }
     score(&grid)
 }
 
-fn part_2(_parsed: &Parsed) -> usize {
-    todo!()
+fn part_2((grid, instr): &Parsed) -> i32 {
+    part_2_expanded(expand(grid), instr)
+}
+
+// separate function for testing
+fn part_2_expanded(mut grid: Grid2D<char>, instr: &Vec<Point<2>>) -> i32 {
+    let mut robot = grid.position(|c| c == &'@').unwrap();
+
+    for dir in instr {
+        let moved = if dir.y() == 0 {
+            move_simple(&mut grid, robot, dir)
+        } else {
+            move_wide(&mut grid, robot, *dir)
+        };
+        if moved {
+            robot += dir;
+        }
+    }
+    score(&grid)
+}
+
+fn move_wide(grid: &mut Grid2D<char>, robot: P, dir: P) -> bool {
+    let mut moving_points = vec![];
+    let mut candidates: VecDeque<_> = [robot].into();
+    while let Some(cand) = candidates.pop_front() {
+        if moving_points.contains(&cand) {
+            // Each of the two sides of a block
+            // can cause the upper block to be inserted twice
+            // if they are perfectly in line.
+            continue;
+        }
+        let check_pt = cand + dir;
+        match grid.get(&check_pt).unwrap() {
+            &'#' => return false,
+            &'.' => (),
+            &'@' => candidates.push_back(check_pt),
+            &'[' => {
+                candidates.push_back(check_pt);
+                candidates.push_back(check_pt + P::E);
+            }
+            &']' => {
+                candidates.push_back(check_pt + P::W);
+                candidates.push_back(check_pt);
+            }
+            c => panic!("Unexpected char {c}"),
+        }
+        moving_points.push(cand);
+    }
+    // `moving_points` are effectively ordered right now,
+    // so we can just iterate backwards.
+    for pt in moving_points.into_iter().rev() {
+        grid.swap(&pt, &(pt + dir));
+    }
+    true
+}
+
+fn move_simple(grid: &mut Grid2D<char>, robot: P, dir: &P) -> bool {
+    let Some(free_i) = (1i32..)
+        .into_iter()
+        .map_while(|i| {
+            grid.get(&(robot + dir * i))
+                .filter(|c| c != &&'#')
+                .map(|b| (i, b))
+        })
+        .find_map(|(i, b)| (b == &'.').then_some(i))
+    else {
+        return false;
+    };
+    for (i1, i2) in (0..=free_i).rev().tuple_windows() {
+        let (pt1, pt2) = (robot + dir * i1, robot + dir * i2);
+        grid.swap(&pt1, &pt2);
+    }
+    true
 }
 
 fn score(grid: &Grid2D<char>) -> i32 {
     grid.iter_enumerate()
-        .filter_map(|(pt, b)| (b == &'O').then_some(pt))
+        .filter_map(|(pt, b)| (b == &'O' || b == &'[').then_some(pt))
         .map(|pt| pt.x() + 100 * pt.y())
         .sum()
+}
+
+fn expand(grid: &Grid2D<char>) -> Grid2D<char> {
+    grid.rows
+        .iter()
+        .map(|row| {
+            row.iter().flat_map(|c| match c {
+                &'.' => ['.', '.'],
+                &'@' => ['@', '.'],
+                &'O' => ['[', ']'],
+                &'#' => ['#', '#'],
+                c => panic!("Unmappable character {c}"),
+            })
+        })
+        .collect()
 }
 
 #[cfg(test)]
@@ -110,10 +182,43 @@ mod tests {
         <^^>>>vv<v>>v<<\n\
         ";
 
+    const TEST_INPUT_SMALL_2: &str = "\
+        #######\n\
+        #...#.#\n\
+        #.....#\n\
+        #..OO@#\n\
+        #..O..#\n\
+        #.....#\n\
+        #######\n\
+        \n\
+        <vv<<^^<<^^\n\
+        ";
+
     test!(part_1() == 10092);
-    test!(small, TEST_INPUT_SMALL, part_1() == 2028);
-    // test!(part_2() == 0);
+    test!(part_2() == 9021);
     bench_parse!(|p: &Parsed| (p.0.size, p.1.len()), (Size(50, 50), 20000));
     bench!(part_1() == 1471826);
-    // bench!(part_2() == 0);
+    bench!(part_2() == 1457703);
+
+    test!(small, TEST_INPUT_SMALL, part_1() == 2028);
+    test!(small, TEST_INPUT_SMALL_2, part_2() == (100 * 1 + 5) + (100 * 2 + 7) + (100 * 3 + 6));
+
+    #[test]
+    fn part_2_special() {
+        let grid_s: &str = "\
+            ####################\n\
+            ##....[]....[]..[]##\n\
+            ##............[]..##\n\
+            ##..[][]....[]..[]##\n\
+            ##...[].......[]..##\n\
+            ##[]##....[]......##\n\
+            ##[]......[]..[]..##\n\
+            ##..[][]..@[].[][]##\n\
+            ##........[]......##\n\
+            ####################\n\
+            ";
+        let grid = grid_s.lines().map(str::chars).collect();
+        let instr = vec![P::N];
+        assert_eq!(part_2_expanded(grid, &instr), 9604);
+    }
 }
