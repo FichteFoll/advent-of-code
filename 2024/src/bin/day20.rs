c8892ed8c4525d138693ec736f4a2b3eb4fc7705
FichteFoll <fichtefoll2@googlemail.com>
2024-12-21 01:03:35 +0100

    2024 day 20 part 2 (wip)

--- 2024/src/bin/day20.rs
+++ 2024/src/bin/day20.rs
@@ -30,30 +30,44 @@ fn part_2(grid: &Parsed, min_saved: I) -> usize {
 
 #[derive(Ord, PartialOrd, PartialEq, Eq, Debug)]
 struct State {
-    // prioritize the no-cheats solution as an early cut-off point
-    cheated: usize,
+    // Prioritize the no-cheats solution as an early cut-off point
+    cheat: Cheat,
+    // Priorizite shortest possible task (estimated)
     min_steps: I,
+    // Prioritize longest current path (to get a "good" solution early)
     steps: Reverse<I>,
-    // rest is irrelevant for ordering
+    // Position is irrelevant for ordering
     pos: P,
-    cheats: Vec<P>,
+}
+
+#[derive(Ord, PartialOrd, PartialEq, Eq, Debug, Clone, Copy, Hash)]
+enum Cheat {
+    None,
+    // TODO consider if different order makes it faster
+    Active(usize, P),
+    Done(P, P),
+}
+
+impl Cheat {
+    fn is_done(&self) -> bool {
+        if let Cheat::Done(..) = self { true } else { false }
+    }
 }
 
 fn count_paths_with_cheats(grid: &Parsed, min_saved: I, allowed_cheats: usize) -> usize {
     let end = grid.position(|c| c == &'E').unwrap();
     let start = grid.position(|c| c == &'S').unwrap();
-    let mut cache: HashMap<(P, Vec<P>), I> = Default::default();
+    let mut cache: HashMap<(P, Cheat), I> = Default::default();
     let mut queue: BTreeSet<State> = [State {
-        cheated: 0,
+        cheat: Cheat::None,
         min_steps: start.manhattan_to(&end),
         steps: Reverse(0),
         pos: start,
-        cheats: Vec::with_capacity(2),
     }]
     .into();
-    let no_cheats_key = (end, vec![]);
+    let no_cheats_key = (end, Cheat::None);
     while let Some(s) = queue.pop_first() {
-        let entry = cache.entry((s.pos, s.cheats.clone())).or_insert(I::MAX);
+        let entry = cache.entry((s.pos, s.cheat)).or_insert(I::MAX);
         if *entry <= s.steps.0 {
             continue;
         }
@@ -62,7 +76,7 @@ fn count_paths_with_cheats(grid: &Parsed, min_saved: I, allowed_cheats: usize) -
             println!("solution: {:?}", s);
             continue;
         }
-        if let Some(&shortest) = cache.get(&(end, s.cheats.clone()))
+        if let Some(&shortest) = cache.get(&(end, s.cheat))
             && shortest < s.min_steps
         {
             continue;
@@ -78,7 +92,7 @@ fn count_paths_with_cheats(grid: &Parsed, min_saved: I, allowed_cheats: usize) -
     let reference = cache.remove(&no_cheats_key).unwrap();
     cache
         .into_iter()
-        .filter_map(|((pt, _), v)| (pt == end).then_some(v))
+        .filter_map(|((pt, c), v)| (pt == end && c.is_done()).then_some(v))
         .filter(|&length| min_saved <= reference - length)
         .count()
 }
@@ -89,31 +103,34 @@ fn successor_states(
     allowed_cheats: usize,
     end: Point<2>,
 ) -> impl Iterator<Item = State> {
-    let can_cheat = s.cheats.len() == 0;
-    s.pos.direct_neighbors().into_iter().filter_map(move |pos| {
-        let mut is_cheating = s.cheats.len() == 1;
-        let is_wall = grid.get(&pos)? == &'#';
-        let mut cheats = s.cheats.clone();
-        match (is_wall, can_cheat, is_cheating) {
-            (_, _, true) if s.cheated == allowed_cheats => return None,
-            (true, true, _) => {
-                cheats.push(s.pos);
-                is_cheating = true;
+    s.pos.direct_neighbors().into_iter().flat_map(move |pos| {
+        let Some(tile) = grid.get(&pos) else {
+            return vec![];
+        };
+        let is_wall = tile == &'#';
+        let cheats = match (is_wall, s.cheat) {
+            (_, Cheat::Active(count, _)) if count == allowed_cheats => vec![],
+            (true, Cheat::None) => vec![Cheat::Active(1, s.pos)],
+            (true, Cheat::Done(..)) => vec![],
+            (true, Cheat::Active(count, start)) => {
+                vec![Cheat::Active(count + 1, start)]
             }
-            (false, _, true) => {
-                cheats.push(pos);
-                is_cheating = false;
+            (false, Cheat::Active(count, start)) => {
+                vec![Cheat::Active(count + 1, start), Cheat::Done(start, pos)]
             }
-            (true, false, false) => return None,
-            _ => (),
-        }
-        Some(State {
-            cheated: s.cheated + is_cheating as usize,
-            min_steps: s.steps.0 + 1 + pos.manhattan_to(&end),
-            steps: Reverse(s.steps.0 + 1),
-            pos,
-            cheats,
-        })
+            (false, c) => vec![c],
+        };
+        let min_steps = s.steps.0 + 1 + pos.manhattan_to(&end);
+        let steps = Reverse(s.steps.0 + 1);
+        cheats
+            .into_iter()
+            .map(|cheat| State {
+                cheat,
+                min_steps,
+                steps,
+                pos,
+            })
+            .collect()
     })
 }
 
