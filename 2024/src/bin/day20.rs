449182d4d67779dc8bbd03aef5b274b7cfd9c495
FichteFoll <fichtefoll2@googlemail.com>
2024-12-24 13:26:13 +0100

    2024 day 20 part 2

--- 2024/src/bin/day20.rs
+++ 2024/src/bin/day20.rs
@@ -31,8 +31,6 @@ fn part_2(grid: &Parsed, min_saved: I) -> usize {
 
 #[derive(Ord, PartialOrd, PartialEq, Eq, Debug)]
 struct State {
-    // Prioritize the no-cheats solution as an early cut-off point
-    cheat: Cheat,
     // Priorizite shortest possible task (estimated)
     min_steps: I,
     // Prioritize longest current path (to get a "good" solution early)
@@ -41,113 +39,53 @@ struct State {
     pos: P,
 }
 
-#[derive(Ord, PartialOrd, PartialEq, Eq, Debug, Clone, Copy, Hash)]
-enum Cheat {
-    None,
-    // TODO consider if different order makes it faster
-    Active(usize, P),
-    Done(P, P),
-}
-
-impl Cheat {
-    fn is_done(&self) -> bool {
-        if let Cheat::Done(..) = self {
-            true
-        } else {
-            false
-        }
-    }
-}
-
-fn count_paths_with_cheats(grid: &Parsed, min_saved: I, allowed_cheats: usize) -> usize {
+fn count_paths_with_cheats(grid: &Parsed, min_saved: I, allowed_cheats: I) -> usize {
     let end = grid.position(|c| c == &'E').unwrap();
     let start = grid.position(|c| c == &'S').unwrap();
-    let mut cache: HashMap<(P, Cheat), I> = Default::default();
+    // Save the minimum amount of steps to finish for each tile.
+    // using a BFS from the end.
+    let cache = bfs(grid, end, start);
+    let reference = *cache.get(&start).unwrap();
+    // Each cheat starts on a valid tile and ends on a floor tile
+    // within a manhattan distance of N,
+    // so we look at all valid permutations of floor tiles
+    // and filter by the calculated saved steps.
+    cache
+        .iter()
+        .tuple_combinations()
+        .filter_map(|((p1, &d1), (p2, &d2))| {
+            let dist = p1.manhattan_to(p2) as I;
+            (dist != 0 && dist <= allowed_cheats).then_some(d1.abs_diff(d2) as I - dist)
+        })
+        .filter(|&saved| saved >= min_saved)
+        .count()
+}
+
+fn bfs(grid: &Grid2D<char>, start: Point<2>, end: Point<2>) -> HashMap<Point<2>, I> {
+    let mut cache: HashMap<P, I> = Default::default();
     let mut queue: BTreeSet<State> = [State {
-        cheat: Cheat::None,
         min_steps: start.manhattan_to(&end),
         steps: Reverse(0),
         pos: start,
     }]
     .into();
-    let no_cheats_key = (end, Cheat::None);
     while let Some(s) = queue.pop_first() {
-        let entry = cache.entry((s.pos, s.cheat)).or_insert(I::MAX);
+        let entry = cache.entry(s.pos).or_insert(I::MAX);
         if *entry <= s.steps.0 {
             continue;
         }
         *entry = s.steps.0;
-        if s.pos == end {
-            println!("solution: {:?}", s);
-            continue;
-        }
-        if let Some(&shortest) = cache.get(&(end, s.cheat))
-            && shortest < s.steps.0 // TODO why does < vs <= make such a difference
-        {
-            continue;
-        }
-        if let Some(&shortest_no_cheats) = cache.get(&no_cheats_key)
-            && shortest_no_cheats < s.min_steps + min_saved
-        {
-            continue;
-        }
-        queue.extend(successor_states(grid, allowed_cheats, end, s));
-    }
-    dbg!(cache.len());
-    let reference = cache.remove(&no_cheats_key).unwrap();
-    cache
-        .into_iter()
-        // .filter_map(|((pt, c), v)| (pt == end && c.is_done()).then_some(v))
-        // .filter(|&length| reference - length >= min_saved)
-        // .count()
-        .filter_map(|((pt, c), v)| (pt == end && c.is_done()).then_some(v))
-        .map(|length| reference - length)
-        .filter(|saved| *saved >= min_saved)
-        .counts()
-        .into_iter()
-        .sorted_unstable_by_key(|(k, _)| *k)
-        .map(|(k, v)| {
-            println!("{v} cheats that save {k} picoseconds");
-            v
-        })
-        .sum()
-}
-
-fn successor_states(
-    grid: &Grid2D<char>,
-    allowed_cheats: usize,
-    end: Point<2>,
-    s: State,
-) -> impl Iterator<Item = State> {
-    s.pos.direct_neighbors().into_iter().flat_map(move |pos| {
-        let Some(tile) = grid.get(&pos) else {
-            return vec![];
-        };
-        let is_wall = tile == &'#';
-        let cheats = match (is_wall, s.cheat) {
-            (_, Cheat::Active(count, _)) if count == allowed_cheats => vec![],
-            (true, Cheat::None) => vec![Cheat::Active(1, s.pos)],
-            (true, Cheat::Done(..)) => vec![],
-            (true, Cheat::Active(count, start)) => {
-                vec![Cheat::Active(count + 1, start)]
-            }
-            (false, Cheat::Active(count, start)) => {
-                vec![Cheat::Active(count + 1, start), Cheat::Done(start, pos)]
-            }
-            (false, c) => vec![c],
-        };
-        let min_steps = s.steps.0 + 1 + pos.manhattan_to(&end);
-        let steps = Reverse(s.steps.0 + 1);
-        cheats
-            .into_iter()
-            .map(|cheat| State {
-                cheat,
-                min_steps,
-                steps,
+        let neighbors = s.pos.direct_neighbors().into_iter();
+        let successors = neighbors
+            .filter(|pos| grid.get(pos).is_some_and(|c| c != &'#'))
+            .map(move |pos| State {
+                min_steps: s.steps.0 + 1 + pos.manhattan_to(&end),
+                steps: Reverse(s.steps.0 + 1),
                 pos,
-            })
-            .collect()
-    })
+            });
+        queue.extend(successors);
+    }
+    cache
 }
 
 #[cfg(test)]
@@ -178,6 +116,6 @@ mod tests {
     test!(part_1(2) == 44);
     test!(part_2(50) == 285);
     bench_parse!(|p: &Parsed| p.size, Size(141, 141));
-    // bench!(part_1(100) == 1327); // takes 1.5 minutes to compute
-    // bench!(part_2(100) == 0);
+    bench!(part_1(100) == 1327);
+    bench!(part_2(100) == 985737);
 }
