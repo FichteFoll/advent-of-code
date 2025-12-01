53ca34f41e1e8cd4355f8d372079d99a511831d6
FichteFoll <fichtefoll2@googlemail.com>
2025-12-01 21:14:32 +0100

    2025 day 01 part 1

---
+++ 2025/haskell/day01/Main.hs
@@ -0,0 +1,27 @@
+{-# OPTIONS_GHC -W #-}
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+
+module Main (main, parse, part1, part2) where
+
+import Control.Arrow ((&&&), Arrow (second), (<<<))
+
+type Input = [(Char, Int)]
+
+
+main :: IO ()
+main = do
+  input <- parse <$> getContents
+  putStrLn $ "Part 1: " ++ show (part1 input)
+  putStrLn $ "Part 2: " ++ show (part2 input)
+
+parse :: String -> Input
+parse = map (second read <<< (head &&& tail)) . lines
+
+part1 :: Input -> Int
+part1 = length . filter (== 0) . scanl (\a (c, n) -> op c a n `mod` 100) 50
+  where
+    op 'L' = (-)
+    op 'R' = (+)
+
+part2 :: Input -> Int
+part2 _ = 0
---
+++ 2025/haskell/day01/MainSpec.hs
@@ -0,0 +1,45 @@
+module MainSpec (main) where
+
+import Test.Hspec
+import Main hiding (main)
+
+exampleText
+  = unlines
+    [ "L68"
+    , "L30"
+    , "R48"
+    , "L5"
+    , "R60"
+    , "L55"
+    , "L1"
+    , "L99"
+    , "R14"
+    , "L82"
+    ]
+
+main :: IO ()
+main = do
+  input <- parse <$> readFile "../../input/day01.txt"
+  let exampleInput = parse exampleText
+
+  hspec $ do
+    describe "parse" $ do
+      it "parses example input (length)" $
+        length exampleInput `shouldBe` 10
+
+      it "parses real input (length)" $
+        length input `shouldBe` 4543
+
+    describe "part1" $ do
+      it "computes accepted result for example input" $
+        part1 exampleInput `shouldBe` 3
+
+      it "computes accepted result" $
+        part1 input `shouldBe` 1135
+
+    -- describe "part2" $ do
+    --   it "computes accepted result for example input" $
+    --     part2 exampleInput `shouldBe` 0
+
+    --   it "computes accepted result" $
+    --     part2 input `shouldBe` 0
