39225983171d0e891494f17aa0f3ca1a64186c75
FichteFoll <fichtefoll2@googlemail.com>
2025-12-14 23:49:55 +0100

    2024 day 04 part 2

--- 2025/haskell/day04/Main.hs
+++ 2025/haskell/day04/Main.hs
@@ -5,6 +5,7 @@ module Main (main, parse, part1, part2) where
 import qualified Data.Set as S
 import Linear.V2
 
+-- First element only for parsing assertions.
 type Input = (Int, S.Set (V2 Int))
 
 main :: IO ()
@@ -20,10 +21,14 @@ parse grid = (dim, S.fromList [V2 x y | (i, c) <- zip [0 ..] $ concat rows, c ==
     dim = length rows
 
 part1 :: Input -> Int
-part1 (_, rolls) = length $ filter ((< 4) . length . filter (`S.member` rolls) . neighbors) $ S.toList rolls
+part1 = length . findRemovables . snd
 
 part2 :: Input -> Int
-part2 _ = 0
+part2 (_, rolls) = length rolls - numRemoved
+  where numRemoved = fst $ head $ dropWhile (uncurry (/=)) $ (zip =<< tail) $ map length $ iterate (\s -> s S.\\ findRemovables s) rolls
 
 neighbors :: V2 Int -> [V2 Int]
 neighbors v = [v + V2 x y | x <- [-1 .. 1], y <- [-1 .. 1], x /= 0 || y /= 0]
+
+findRemovables :: S.Set (V2 Int) -> S.Set (V2 Int)
+findRemovables rolls = S.filter ((< 4) . length . filter (`S.member` rolls) . neighbors) rolls
--- 2025/haskell/day04/MainSpec.hs
+++ 2025/haskell/day04/MainSpec.hs
@@ -37,9 +37,9 @@ main = do
       it "computes accepted result" $
         part1 input `shouldBe` 1486
 
-    -- describe "part2" $ do
-    --   it "computes accepted result for example input" $
-    --     part2 exampleInput `shouldBe` 0
+    describe "part2" $ do
+      it "computes accepted result for example input" $
+        part2 exampleInput `shouldBe` 43
 
-    --   it "computes accepted result" $
-    --     part2 input `shouldBe` 0
+      it "computes accepted result" $
+        part2 input `shouldBe` 9024
