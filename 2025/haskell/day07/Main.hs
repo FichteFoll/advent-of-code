6171af9fcf88e436a6a6ee2ffea40651659ff3f7
FichteFoll <fichtefoll2@googlemail.com>
2025-12-30 00:01:51 +0100

    2025 day 07: import elemIndices

--- 2025/haskell/day07/Main.hs
+++ 2025/haskell/day07/Main.hs
@@ -5,6 +5,7 @@ module Main (main, parse, part1, part2) where
 import qualified Data.IntSet as S
 import qualified Data.IntMap.Strict as M
 import Control.Arrow
+import Data.List (elemIndices)
 
 type Input = (Int, [S.IntSet])
 
@@ -16,8 +17,8 @@ main = do
 
 parse :: String -> Input
 parse
-  = head . posWhereEq 'S' . head
-  &&& filter (not . S.null) . map (S.fromDistinctAscList . posWhereEq '^') . tail
+  = head . elemIndices 'S' . head
+  &&& filter (not . S.null) . map (S.fromDistinctAscList . elemIndices '^') . tail
   <<< lines
 
 part1 :: Input -> Int
@@ -28,7 +29,7 @@ part2 = sum . M.elems . snd . solve
 
 -- We can compute both solutions in a single iteration.
 -- Part 1 could be optimized if done separately, but this saves code.
-solve :: (Foldable t, Num a) => (M.Key, t S.IntSet) -> (Int, M.IntMap a)
+solve :: (Foldable t, Num a) => (Int, t S.IntSet) -> (Int, M.IntMap a)
 solve (startPos, grid) = foldl lineStep (0, M.singleton startPos 1) grid
 
 lineStep :: Num a => (Int, M.IntMap a) -> S.IntSet -> (Int, M.IntMap a)
@@ -41,6 +42,3 @@ lineStep (c, timelineMap) splitters
     changed = M.restrictKeys timelineMap changeKeys
     splitLeft = M.mapKeysMonotonic pred changed
     splitRight = M.mapKeysMonotonic succ changed
-
-posWhereEq :: Eq b => b -> [b] -> [Int]
-posWhereEq c = map fst . filter ((== c) . snd) . zip [0..]
